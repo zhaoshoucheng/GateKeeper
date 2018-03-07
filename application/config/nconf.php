cb63e89c88c918a86599cc9844e7718eb1036148
ningxiangbing <ningxiangbing@didichuxing.com>
2018-03-07 18:07:18 +0800

    添加 指标配置 置信度配置

--- application/config/nconf.php
+++ application/config/nconf.php
@@ -16,3 +16,33 @@ $config['task_interface'] = 'http://' . $task_server . ':' . $task_port;
 
 $config['task_type'] = [0=>'周期任务', 1=>'自定义任务'];
 
+// 置信度
+$config['confidence'] = [
+							1=>[
+								'name'		=> '高',
+								'expression'=> '>=50'
+							],
+							2=>[
+								'name'		=> '低',
+								'expression'=> '<50'
+							]
+						];
+
+// 路口指标
+$config['junction_quota_key'] = [
+									'imbalance_index'		=> '失衡指数',
+									'spillover_index'		=> '溢流指数',
+									'incoordination_index'	=> '失调指数',
+									'saturation_index'		=> '饱和指数'
+								];
+
+// flow指标
+$config['flow_quota_key'] = [
+								'stop_delay'		=> '停车延误',
+								'saturation_degree'	=> '饱和度',
+								'queue_position'	=> '排队长度',
+								'stop_time_cycle'	=> '停车周期次数'
+							];
+
+
+
