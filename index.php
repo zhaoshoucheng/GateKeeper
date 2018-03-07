27790d48df13195119d6d8afa7a0858687866ed1
lizhaohua <lizhaohua@didichuxing.com>
2018-03-07 10:25:47 +0800

    check ENVIRONMENT with online_host

--- index.php
+++ index.php
@@ -53,7 +53,20 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+$online_host = array(
+	'ipd-cloud-internal-web00.gz01',
+	'ipd-cloud-internal-web01.gz01',
+	'ipd-cloud-server00.gz01',
+	'ipd-cloud-server01.gz01',
+);
+$hostname = gethostname();
+if (in_array($hostname, $online_host)) {
+	define('ENVIRONMENT', 'production');
+}else{
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+}
 
 /*
  *---------------------------------------------------------------
