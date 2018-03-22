b876cb75d3594b23ea32bd6f3c9704f124ee84f5
ningxiangbing <ningxiangbing@didichuxing.com>
2018-03-22 11:42:53 +0800

    delete debug
    fix bug

--- application/controllers/Junction.php
+++ application/controllers/Junction.php
@@ -39,11 +39,6 @@ class Junction extends MY_Controller {
 			return $this->response([], 100400, $validate['errmsg']);
 		}
 
-		if($this->debug){
-			$params['task_id'] = 1090;
-			$params['city_id'] = 12;
-		}
-
 		$data['task_id'] = (int)$params['task_id'];
 		$data['type'] = (int)$params['type'];
 		$data['city_id'] = $params['city_id'];
@@ -96,13 +91,6 @@ class Junction extends MY_Controller {
 			]
 		);
 
-		// 测试做假数据用，真实数据出来后删除
-		if($this->debug){
-			$params['task_id'] = 1090;
-			$params['dates'] = ['20180105', '20180106'];
-			$params['time_point'] = '05:00';
-		}
-
 		if(!$validate['status']){
 			return $this->response([], 100400, $validate['errmsg']);
 		}
@@ -138,13 +126,6 @@ class Junction extends MY_Controller {
 		);
 		if(!$validate['status']){
 			return $this->response([], 100400, $validate['errmsg']);
-		}
-
-		// 测试做假数据用，真实数据出来后删除
-		if($this->debug){
-			$params['junction_id'] = '2017030116_5546361';
-			$params['dates'] = ['20180105', '20180106'];
-			$params['time_point'] = '05:00';
 		}
 
 		if(!is_array($params['dates']) || count($params['dates']) < 1){
@@ -196,13 +177,6 @@ class Junction extends MY_Controller {
 		);
 		if(!$validate['status']){
 			return $this->response([], 100400, $validate['errmsg']);
-		}
-
-		// 测试做假数据用，真实数据出来后删除
-		if($this->debug){
-			$params['task_id'] = 1090;
-			$params['city_id'] = 12;
-			$params['time_point'] = '05:00';
 		}
 
 		$data['task_id'] = (int)$params['task_id'];
@@ -260,13 +234,6 @@ class Junction extends MY_Controller {
 			return $this->response([], 100400, $validate['errmsg']);
 		}
 
-		// 测试做假数据用，真实数据出来后删除
-		if($this->debug){
-			$params['task_id'] = 1090;
-			$params['city_id'] = 12;
-			$params['time_point'] = '05:00';
-		}
-
 		$data['orderby'] = 2;
 		if(isset($params['orderby']) && (int)$params['orderby'] == 1){
 			$data['orderby'] = 1;
@@ -312,13 +279,6 @@ class Junction extends MY_Controller {
 		);
 		if(!$validate['status']){
 			return $this->response([], 100400, $validate['errmsg']);
-		}
-
-		// 测试做假数据用，真实数据出来后删除
-		if($this->debug){
-			$params['junction_id'] = '2017030116_5546361';
-			$params['dates'] = ['20180105', '20180106'];
-			$params['time_point'] = '05:00';
 		}
 
 		if(!is_array($params['dates']) || count($params['dates']) < 1){
@@ -404,7 +364,6 @@ class Junction extends MY_Controller {
 	*/
 	public function testLogin(){
 		echo "username = " . $this->username;
-		echo "<hr>host = " . $_SERVER['HTTP_HOST'];
 		exit;
 	}
 
