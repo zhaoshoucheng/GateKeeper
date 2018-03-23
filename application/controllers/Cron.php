b22e04c5e783b3f81bfd4190f516fcdf2c5bc6f4
lizhaohua <lizhaohua@didichuxing.com>
2018-03-23 10:46:24 +0800

    sleep

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -35,10 +35,10 @@ class Cron extends CI_Controller {
 			$task = $this->task_model->process();
 			if ($task === true or $task === false) {
 				$i ++;
-				if ($i === 5) {
+				if ($i === 2) {
 					break;
 				}
-				sleep(2 * 60);
+				sleep(5 * 60);
 			} else {
 				print_r($task);
 				try {
