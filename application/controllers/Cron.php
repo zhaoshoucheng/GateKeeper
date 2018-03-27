21dfe55b6bceb8371b711cad8cac6f775a1477bb
lizhaohua <lizhaohua@didichuxing.com>
2018-03-27 13:57:31 +0800

    cron test

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -51,11 +51,11 @@ class Cron extends CI_Controller {
 					$hdfs_dir = "/user/its_bi/its_flow_tool/{$task_id}_{$trace_id}/";
 					$this->task_model->updateTask($task['id'], ['trace_id' => $trace_id]);
 					// process_flow
-					$task = new Task();
-					$response = $task->areaFlowProcess($city_id, $task_id, $trace_id, $hdfs_dir, array_values(array_unique($dateVersion)));
+					$taskService = new Task();
+					$response = $taskService->areaFlowProcess($city_id, $task_id, $trace_id, $hdfs_dir, array_values(array_unique($dateVersion)));
 					print_r($response);
 					// process_index
-					$response = $task->calculate($city_id, $task_id, $trace_id, $hdfs_dir, $start_time . ':00', $end_time . ':00', $dateVersion);
+					$response = $taskService->calculate($city_id, $task_id, $trace_id, $hdfs_dir, $start_time . ':00', $end_time . ':00', $dateVersion);
 					print_r($response);
 				} catch (\Exception $e) {
 					$this->task_model->updateTask($task['id'], ['status' => -1, 'task_end_time' => time()]);
