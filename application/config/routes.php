cb31f743a66c1b2fb32cff65af3b1b634a583f1e
叶剑峰 <yejianfeng@didichuxing.com>
2018-03-28 22:41:12 +0800

    add route online

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['itstool/(.+)'] = "$1";
+$route['signalpro/api/(.+)'] = "$1";
