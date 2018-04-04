f728504db5dbecbead7ba831d15f34b0627bf312
ningxiangbing <ningxiangbing@didichuxing.com>
2018-04-04 11:48:31 +0800

    add

---
+++ application/config/mail.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['mail'] = array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'mail.didichuxing.com',
+    'smtp_user' => 'sts_traffic@didichuxing.com',
+    'smtp_pass' => 'JGTvn0XP',
+    'smtp_port' => 587,
+    'smtp_crypto' => 'tls',
+    'protocol' => 'sendmail',
+);
