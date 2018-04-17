35305bb94424bd63ea12cf6ed3c47d0b069efc6c
ningxiangbing <ningxiangbing@didichuxing.com>
2018-04-17 16:31:17 +0800

    dev

--- application/controllers/Track.php
+++ application/controllers/Track.php
@@ -183,15 +183,15 @@ class Track extends MY_Controller {
 				}else if(strstr($v, '|') !== false && strstr($v, '#') === false){
 					$temp_arr = explode("|", $v);
 					$sample_data[$k]['xType'] = 3;
-					$sample_data[$k]['xType']['xData']['lR'] = $temp_arr[0];
-					$sample_data[$k]['xType']['xData']['rR'] = $temp_arr[1];
+					$sample_data[$k]['xData']['lR'] = $temp_arr[0];
+					$sample_data[$k]['xData']['rR'] = $temp_arr[1];
 				}else if(strstr($v, '|') !== false && strstr($v, '#') !== false){
 					$temp_arr = explode('#', $v);
 					$temp_val = explode('|', $temp_arr[1]);
 					$sample_data[$k]['xType'] = 4;
 					$sample_data[$k]['xData']['noStop'] = 1;
-					$sample_data[$k]['xType']['xData']['lR'] = $temp_val[0];
-					$sample_data[$k]['xType']['xData']['rR'] = $temp_val[1];
+					$sample_data[$k]['xData']['lR'] = $temp_val[0];
+					$sample_data[$k]['xData']['rR'] = $temp_val[1];
 				}else{
 					$sample_data[$k]['xType'] = 1;
 					$sample_data[$k]['xData']['all'] = true;
@@ -203,15 +203,15 @@ class Track extends MY_Controller {
 				}else if(strstr($af_condition[$k], '|') !== false && strstr($af_condition[$k], '#') === false){
 					$temp_arr = explode("|", $af_condition[$k]);
 					$sample_data[$k]['yType'] = 3;
-					$sample_data[$k]['yType']['yData']['lR'] = $temp_arr[0];
-					$sample_data[$k]['yType']['yData']['rR'] = $temp_arr[1];
+					$sample_data[$k]['yData']['lR'] = $temp_arr[0];
+					$sample_data[$k]['yData']['rR'] = $temp_arr[1];
 				}else if(strstr($af_condition[$k], '|') !== false && strstr($af_condition[$k], '#') !== false){
 					$temp_arr = explode('#', $af_condition[$k]);
 					$temp_val = explode('|', $temp_arr[1]);
 					$sample_data[$k]['yType'] = 4;
 					$sample_data[$k]['yData']['noStop'] = 1;
-					$sample_data[$k]['yType']['yData']['lR'] = $temp_val[0];
-					$sample_data[$k]['yType']['yData']['rR'] = $temp_val[1];
+					$sample_data[$k]['yData']['lR'] = $temp_val[0];
+					$sample_data[$k]['yData']['rR'] = $temp_val[1];
 				}else{
 					$sample_data[$k]['yType'] = 1;
 					$sample_data[$k]['yData']['all'] = true;
