a739891584ef8c981eae3b3cbe4fe6ff8fd26176
ningxiangbing <ningxiangbing@didichuxing.com>
2018-04-25 20:20:20 +0800

    修改散点图X.Y轴最大最小值

--- application/models/Track_model.php
+++ application/models/Track_model.php
@@ -299,7 +299,10 @@ class Track_model extends CI_Model
 
         $result_data['info']['id'] = trim($junction_info['flow_id']);
         $result_data['info']['comment'] = $timing['comment'];
-        $result_data['info']['y_max'] = (int)$timing['cycle'] * 2;
+        $result_data['info']['x']['min'] = $junction_info['start_time'];
+        $result_data['info']['x']['max'] = $junction_info['end_time'];
+        $result_data['info']['y']['max'] = 0;
+        $result_data['info']['y']['max'] = (int)$timing['cycle'] * 2;
         return $result_data;
     }
 }
