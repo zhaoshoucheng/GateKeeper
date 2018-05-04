7db6755f95608462be33fccdb49b14cf5b8a45c5
ningxiangbing <ningxiangbing@didichuxing.com>
2018-05-04 13:49:05 +0800

    optimize

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -22,12 +22,6 @@ class MY_Controller extends CI_Controller {
 		date_default_timezone_set('Asia/Shanghai');
 		$host = $_SERVER['HTTP_HOST'];
 
-        $this->load->config('nconf');
-        $back_timing_roll = $this->config->item('back_timing_roll');
-        if (in_array($this->username, $back_timing_roll, true)) {
-            $this->timingType = 2;
-        }
-
 		if ( $host != '100.90.164.31:8088' && $host != 'www.itstool.com') {
 			$this->is_check_login = 1;
 
@@ -63,13 +57,14 @@ class MY_Controller extends CI_Controller {
 	                $this->_output();
 	                exit();
 	            }
-
-                if($this->input->post_get('debug')){
-                    echo "username = " . $this->username . '<br> timingType = ' . $this->timingType;
-                    $this->debug = true;
-                }
 	        }
 		}
+
+        $this->load->config('nconf');
+        $back_timing_roll = $this->config->item('back_timing_roll');
+        if (in_array($this->username, $back_timing_roll, true)) {
+            $this->timingType = 2;
+        }
 	}
 
 	public function response($data, $errno = 0, $errmsg = '') {
