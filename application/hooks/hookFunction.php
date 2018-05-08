931ef9938f97962502a7d623c09a6d022be40132
叶剑峰 <yejianfeng@didichuxing.com>
2018-05-08 20:52:59 +0800

    打开strace设置

--- application/hooks/hookFunction.php
+++ application/hooks/hookFunction.php
@@ -4,6 +4,9 @@ if( !function_exists('pre_controller')){
         $_global_log_id = 0;
         $ci = &get_instance();
         $ci->load->helper('log');
+
+        global $_g_should_enable_strace;
+        $_g_should_enable_strace = true;
         gen_logid();
         gen_traceid();
         log_request();
