0ee30c8a7e7ba75a7a8f9c2332c90bcde1f4660a
叶剑峰 <yejianfeng@didichuxing.com>
2018-05-09 16:40:00 +0800

    add redis config

---
+++ application/config/development/redis.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['redis'] = array(
+    'default'=>array(
+        'timeout'=>0.1,
+        'password'=>NULL,
+        'servers'=>array(
+            array('host'=>"127.0.0.1","port"=>"6379"),
+        )
+    ),
+    'report'=>array(
+        'timeout'=>0.1,
+        'password'=>NULL,
+        'servers'=>array(
+            array('host'=>"127.0.0.1","port"=>"6379"),
+        )
+    ),
+);
