6043cdf84a8a32d956e0cce55613441d397a1779
ningxiangbing <ningxiangbing@didichuxing.com>
2018-06-12 10:22:53 +0800

    新增时段优化类

---
+++ application/controllers/Timeframeoptimize.php
@@ -0,0 +1,28 @@
+<?php
+/***************************************************************
+# 时段优化类
+# user:ningxiangbing@didichuxing.com
+# date:2018-06-012
+***************************************************************/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Timeframescatter extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    * 获取单点时段优化路口集合
+    * @param task_id   Y 任务ID
+    * @param city_id   Y 城市ID
+    * @return json
+    */
+    public function getAllJunctions()
+    {
+        
+    }
+
+}
