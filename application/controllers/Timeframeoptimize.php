9ddc159430f8a3850423e1e4166879bd0a9743c5
ningxiangbing <ningxiangbing@didichuxing.com>
2018-06-13 16:51:46 +0800

    dev

--- application/controllers/Timeframeoptimize.php
+++ application/controllers/Timeframeoptimize.php
@@ -27,9 +27,11 @@ class Timeframescatter extends MY_Controller
 
     /**
     * 获取路口相位集合（按NEMA排序）
-    * @param city_id     interger Y 城市ID
-    * @param task_id     interger Y 任务ID
-    * @param junction_id string   Y 路口ID
+    * @param city_id         interger Y 城市ID
+    * @param task_id         interger Y 任务ID
+    * @param junction_id     string   Y 路口ID
+    * @param dates           array    Y 评估/诊断日期
+    * @param task_time_range string   Y 任务时间段
     * @return json
     */
     public function getJunctionMovements()
@@ -49,10 +51,18 @@ class Timeframescatter extends MY_Controller
             return;
         }
 
+        if (!is_array($params['dates']) || count($params['dates']) < 1) {
+            $this->errno = ERR_PARAMETERS;
+            $this->errmsg = 'The dates cannot be empty and must be array.';
+            return;
+        }
+
         $data = [
             'task_id'     => intval($params['task_id']),
-            'city_id'     => intval($params['city_id']),
             'junction_id' => trim($params['junction_id']),
+            'dates'       => trim($params['dates']),
+            'time_range'  => trim($params['task_time_range']),
+            'timingType'  => $this->timingType
         ];
 
         $result = $this->timeframeoptimize->getJunctionMovements($data);
--- application/models/Timeframeoptimize_model.php
+++ application/models/Timeframeoptimize_model.php
@@ -13,4 +13,51 @@ class Timeframeoptimize_model extends CI_Model
         parent::__construct();
         $this->db = $this->load->database('default', true);
     }
+
+    /**
+    * 获取路口相位集合 按nema排序
+    * @param $data['task_id']     interger Y 任务ID
+    * @param $data['junction_id'] string   Y 路口ID
+    * @param $data['dates']       array    Y 评估/诊断日期
+    * @param $data['time_range']  string   Y 任务时间段
+    * @param $data[timingType']   interger Y 配时数据源 0，全部；1，人工；2，配时反推；3，信号机上报
+    * @return array
+    */
+    public function getJunctionMovements($data)
+    {
+        if (empty($data)) {
+            return [];
+        }
+
+        $this->db->select('movements');
+        $this->db->from($this->tb);
+
+        $where = 'task_id = ' . $data['task_id'] . " and junction_id = '{$data['junction_id']}' and type = 0";
+        $this->db->where($where);
+
+        $list = $this->db->get();
+        if (!$list) {
+            return [];
+        }
+
+        $list = $this->db->result_array();
+
+        $result = $this->formatDataForJunctionMovementsByNema($list, $data);
+
+        return $result;
+    }
+
+    /**
+    * 对路口相位集合按nema排序进行格式化
+    * @param $list                array    Y 一个任务一个路口的全任务时段集合
+    * @param $data['task_id']     interger Y 任务ID
+    * @param $data['junction_id'] string   Y 路口ID
+    * @param $data[dates']        array    Y 评估/诊断日期
+    * @param $data[time_range']   string   Y 任务时间段
+    * @param $data[timingType']   interger Y 配时数据源 0，全部；1，人工；2，配时反推；3，信号机上报
+    */
+    private function formatDataForJunctionMovementsByNema($list, $data)
+    {
+        echo "<pre>";print_r($list);
+    }
 }
