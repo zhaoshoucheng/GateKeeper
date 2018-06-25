8678f4f5ecd994b2fd0ffcc4edfce9ea077f669d
ningxiangbing <ningxiangbing@didichuxing.com>
2018-06-25 15:55:59 +0800

    opt

--- application/models/Splitoptimize_model.php
+++ application/models/Splitoptimize_model.php
@@ -132,18 +132,19 @@ class Splitoptimize_model extends CI_Model
 
         // 绿信比优化建议模板
         $splitOptSuggestConf = $this->config->item('split_opt_suggest');
-
+        // 优化建议
+        $result['suggest'] = [];
         foreach ($splitOptSuggestConf as $k=>$v) {
-            if (!empty($res['green_split_opt_signal_plan'][$k])) {
-                foreach ($res['green_split_opt_signal_plan'][$k] as $kk=>$vv) {
+            if (!empty($res[$k])) {
+                foreach ($res[$k] as $kk=>$vv) {
                     $result['suggest'][$k][$kk]
                         = str_replace(':movement', $flowIdName[$vv], $v);
                 }
             }
         }
-        if (empty($result['suggest'])) {
+        /*if (!empty($result['suggest'])) {
             $result['suggest'] = array_values($result['suggest']);
-        }
+        }*/
 
         return $result;
     }
