b7fb0a1d337ff20f19ff7fc0a9caf0d80a4eb17c
ningxiangbing <ningxiangbing@didichuxing.com>
2018-06-25 15:47:30 +0800

    绿信比优化方案接口添加优化建议

--- application/models/Splitoptimize_model.php
+++ application/models/Splitoptimize_model.php
@@ -130,6 +130,21 @@ class Splitoptimize_model extends CI_Model
             }
         }
 
+        // 绿信比优化建议模板
+        $splitOptSuggestConf = $this->config->item('split_opt_suggest');
+
+        foreach ($splitOptSuggestConf as $k=>$v) {
+            if (!empty($res['green_split_opt_signal_plan'][$k])) {
+                foreach ($res['green_split_opt_signal_plan'][$k] as $kk=>$vv) {
+                    $result['suggest'][$k][$kk]
+                        = str_replace(':movement', $flowIdName[$vv], $v);
+                }
+            }
+        }
+        if (empty($result['suggest'])) {
+            $result['suggest'] = array_values($result['suggest']);
+        }
+
         return $result;
     }
 }
