ad5bed046a0d13ad2ddb5adcbe889203ae3cfefb
niuyufu <niuyufu@didichuxing.com>
2018-07-10 14:06:45 +0800

    opt

--- application/models/Waymap_model.php
+++ application/models/Waymap_model.php
@@ -259,7 +259,8 @@ class Waymap_model extends CI_Model
         $qArr['city_id'] = 23;  //先都写23
         $qArr['token'] = $token;
         try {
-            $res = httpGet('https://sts.didichuxing.com/api/signal/link/info', $qArr);
+            $url = 'https://sts.didichuxing.com/api/signal/link/info';
+            $res = httpGet($url, $qArr);
             $retArr = json_decode($res, true);
             if (isset($retArr['errno'])
                 && $retArr['errno'] == 0
@@ -270,44 +271,13 @@ class Waymap_model extends CI_Model
                 throw new \Exception($errorMsg);
             }
         } catch (Exception $e) {
-            com_log_warning('_itstool_waymap_getLinksGeoInfos_error', 0, $e->getMessage(), compact("qArr","res"));
+            com_log_warning('_itstool_waymap_getLinksGeoInfos_error', 0, $e->getMessage(), compact("url","qArr","res"));
             throw new \Exception($e->getMessage());
         }
     }
 
     /**
-     * @deprecated
-     * 新四获取多个links的geo数据
-     *
-     * @param $linksArr     array           linkId数组
-     * @return array
-     */
-    public function getLinksGeoInfosByMap($linksArr){
-        if (!is_array($linksArr) || empty($linksArr)) {
-            return [];
-        }
-        $qArr = [];
-        $qArr['id'] = implode(',', $linksArr);
-        $qArr['version'] = '2017101118';
-        $qArr['origin'] = 0;
-
-        try {
-            $res = httpPOST('http://100.69.187.40:8080/link_query/linkid_with_node', $qArr, 0 , 'raw');
-            $retArr = json_decode($res, true);
-            if (isset($retArr['features'])) {
-                return $retArr;
-            }else{
-                $errorMsg = !empty($retArr['errorMsg']) ? $retArr['errorMsg'] : "The linkid_with_node error format.";
-                throw new \Exception($errorMsg);
-            }
-        } catch (Exception $e) {
-            com_log_warning('_itstool_waymap_getLinksGeoInfosByMap_error', 0, $e->getMessage(), compact("qArr","res"));
-            throw new \Exception($e->getMessage());
-        }
-    }
-
-    /**
-     * 获取某一个路口与相邻路口的flow
+     * 获取某一个路口与相邻路口的links
      * http://wiki.intra.xiaojukeji.com/pages/viewpage.action?pageId=146798263
      *
      * @param $qArr array 请求参数
@@ -315,53 +285,13 @@ class Waymap_model extends CI_Model
      */
     public function getConnectionAdjJunctions($qArr)
     {
-        if(ENVIRONMENT == 'development'){
-            $res = '{
-	"errorCode": 0,
-	"errorMsg": "",
-	"data": {
-		"junctions_info": {
-			"2017030116_5546487": {
-				"name": "经一路-生产路",
-				"lng": "117.01519",
-				"lat": "36.67656",
-				"node_ids": [5546486, 5546487]
-			},
-			"2017030116_5552628": {
-				"name": "经一路-白鹤",
-				"lng": "117.02644",
-				"lat": "36.67869",
-				"node_ids": [5552627, 5552628]
-			},
-			"b26c6af6c3edefe326ce77ab3457515f": {
-				"name": "经一路-北关北路",
-				"lng": "117.01868",
-				"lat": "36.67813",
-				"node_ids": [10935735, 10935736]
-			}
-		},
-		"start_junc_id": "b26c6af6c3edefe326ce77ab3457515f",
-		"adj_junc_paths": [{
-			"end_junc_id": "2017030116_5546487",
-			"links": "128503250,73771671,73771661,704904111,704904101,204355251",
-			"reverse_links": "74488990,73771691,73771681,128503271"
-		}, {
-			"end_junc_id": "2017030116_5552628",
-			"links": "128503261,74374111,74488971,74547200",
-			"reverse_links": "74309850,74430830,74374130,128503240"
-		}]
-	}
-}';
-            $retArr = json_decode($res, true);
-            return $retArr['data'];
-        }
-
         if (!is_array($qArr) || empty($qArr)) {
             throw new \Exception("The qjson empty.");
         }
 
         try {
-            $res = httpPOST($this->config->item('waymap_interface') . '/signal-map/connect/adj_junctions', $qArr, 0, 'json');
+            $url = $this->config->item('waymap_interface') . '/signal-map/connect/adj_junctions';
+            $res = httpPOST($url, $qArr, 0, 'json');
             $retArr = json_decode($res, true);
             if (isset($retArr['errorCode'])
                 && $retArr['errorCode'] == 0
@@ -372,7 +302,7 @@ class Waymap_model extends CI_Model
                 throw new \Exception($errorMsg);
             }
         } catch (Exception $e) {
-            com_log_warning('_itstool_waymap_getConnectionAdjJunctions_error', 0, $e->getMessage(), compact("qArr","res"));
+            com_log_warning('_itstool_waymap_getConnectionAdjJunctions_error', 0, $e->getMessage(), compact("url","qArr","res"));
             throw new \Exception($e->getMessage());
         }
     }
