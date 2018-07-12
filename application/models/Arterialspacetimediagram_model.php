77534a60820cb54f134b1ee19a2e294745f9d949
ningxiangbing <ningxiangbing@didichuxing.com>
2018-07-12 18:36:59 +0800

    干线时空图开发

--- application/models/Arterialspacetimediagram_model.php
+++ application/models/Arterialspacetimediagram_model.php
@@ -16,11 +16,47 @@ class Arterialspacetimediagram_model extends CI_Model
         parent::__construct();
     }
 
+    /**
+    * 获取干线时空图
+    * @param $data['junctions']  array Y 路口信息集合 如下：
+    *   $data['junctions'] = [
+    *       [
+    *           'junction_id'     => '2017030116_5138189', // 路口ID
+    *           'forward_flow_id' => '2017030116_i_73821231_2017030116_o_877944131', // 正向flow
+    *           'reverse_flow_id' => '2017030116_i_877944150_2017030116_o_877944100', // 反向flow
+    *           'tod_start_time'  => '16:00:00', // 配时方案开始时间
+    *           'tod_end_time'    => '19:30:00', // 配时方案结束时间
+    *           'cycle'           => 220         // 配时周期
+    *       ],
+    *   ]
+    * @param $data['task_id']    interger Y 任务ID
+    * @param $data['dates']      array    Y 评估/诊断日期
+    * @param $data['time_point'] string   Y 查询时间点
+    * @param $data['method']     interger Y 0=>正向 1=>反向 2=>双向
+    * @param $data['token']      string   Y 此次请求唯一标识，用于前端轮询
+    * @return array
+    */
     public function getSpaceTimeDiagram($data)
     {
-    	$vals = [];
-    	$server = new Arterialspacetimediagram_vendor();
+    	// 获取map_version
+        $mapversions = $this->taskdateversion_model->select($data['task_id'], $data['dates']);
+        if (!$mapversions) {
+            return [];
+        }
 
-    	$server->getSpaceTimeDiagram($vals);
+        // 组织thrift所需version数组
+        foreach ($mapversions as $k=>$v) {
+            $version[$k]['map_version'] = $v['map_version_md5'];
+            $version[$k]['date'] = $v['date'];
+        }
+
+        $data['version'] = $version;
+
+    	$server = new Arterialspacetimediagram_vendor();
+    	$res = $server->getSpaceTimeDiagram($vals);
+
+        echo "<pre>";print_r($res);exit;
+
+        return $result;
     }
 }
--- vendor/cloud/itsmap/src/Arterialspacetimediagram_vendor.php
+++ vendor/cloud/itsmap/src/Arterialspacetimediagram_vendor.php
@@ -19,58 +19,44 @@ class Arterialspacetimediagram_vendor
 
     /**
     * 获取干线时空图
+    * @param $data['junctions']  array Y 路口信息集合 如下：
+    *   $data['junctions'] = [
+    *       [
+    *           'junction_id'     => '2017030116_5138189', // 路口ID
+    *           'forward_flow_id' => '2017030116_i_73821231_2017030116_o_877944131', // 正向flow
+    *           'reverse_flow_id' => '2017030116_i_877944150_2017030116_o_877944100', // 反向flow
+    *           'tod_start_time'  => '16:00:00', // 配时方案开始时间
+    *           'tod_end_time'    => '19:30:00', // 配时方案结束时间
+    *           'cycle'           => 220         // 配时周期
+    *       ],
+    *   ]
+    * @param $data['version']    array  Y 版本信息 如下：
+    *   $data['version'] = [
+    *       [
+    *           'date' => '20180604',
+    *           'map_version' => '5cbc047bb7cb48efc732dc48307533a8'
+    *       ],
+    *   ]
+    * @param $data['method']     interger Y 0=>正向 1=>反向 2=>双向
+    * @param $data['token']      string   Y 此次请求唯一标识，用于前端轮询
     * @return array
     */
     public function getSpaceTimeDiagram($data) {
-        $arr = [
-            [
-                'junction_id'     => '2017030116_5138189', // 路口ID
-                'forward_flow_id' => '2017030116_i_73821231_2017030116_o_877944131', // 正向flow
-                'reverse_flow_id' => '2017030116_i_877944150_2017030116_o_877944100', // 反向flow
-                'tod_start_time'  => '16:00:00', // 配时方案开始时间
-                'tod_end_time'    => '19:30:00', // 配时方案结束时间
-                'cycle'           => 220      // 配时周期
-            ],
-        ];
-
-        $versionArr = [
-            [
-                'date' => '20180604',
-                'map_version' => '5cbc047bb7cb48efc732dc48307533a8'
-            ],
-            [
-                'date' => '20180601',
-                'map_version' => '5cbc047bb7cb48efc732dc48307533a8'
-            ],
-            [
-                'date' => '20180531',
-                'map_version' => '5cbc047bb7cb48efc732dc48307533a8'
-            ],
-            [
-                'date' => '20180530',
-                'map_version' => '5cbc047bb7cb48efc732dc48307533a8'
-            ],
-            [
-                'date' => '20180529',
-                'map_version' => '2db3490106a19de01f184f3b20756c04'
-            ],
-        ];
-
-        foreach ($versionArr as $v) {
+        // 实例version
+        foreach ($data['version'] as $v) {
             $version[] = new sysVersion($v);
         }
 
-        foreach ($arr as &$v) {
+        // 实例RouteJunction
+        foreach ($data['junctions'] as &$v) {
             $v = new RouteJunction($v);
         }
 
-        $method = 0;
+        $method = $data['method'];
 
         $service = new RoadNet();
-        $response = $service->getSpaceTimeDiagram($arr, $method, $version);
+        $response = $service->getSpaceTimeDiagram($data['junctions'], $method, $version);
 
-        echo "<pre>response = ";print_r($response);
-        echo "<hr><pre>arr = ";print_r($arr);
-        echo "<hr><pre>version = ";print_r($version);
+        return $response;
     }
 }
