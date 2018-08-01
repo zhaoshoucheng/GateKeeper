40eebb46910a8e5713f5694d005e4011879bba40
licxi <lichaoxi_i@didichuxing.com>
2018-08-01 11:52:55 +0800

    适配数据库中时间格式的修改

--- application/models/Overview_model.php
+++ application/models/Overview_model.php
@@ -29,7 +29,7 @@ class Overview_model extends CI_Model
 
         $result = $this->db->select('*')
             ->from($table)
-            ->where('hour', date('H:i', strtotime($data['time_point'])))
+            ->where('hour', $data['time_point'])
             ->where('updated_at >=', $data['date'] . ' 00:00:00')
             ->where('updated_at <=', $data['date'] . ' 23:59:59')
             ->get()->result_array();
