20ea48abcd3a9658a91a21f17edf3029591b03ff
ningxiangbing <ningxiangbing@didichuxing.com>
2018-08-07 11:47:09 +0800

    dev getJunctionQuotaSortList

--- application/models/Evaluate_model.php
+++ application/models/Evaluate_model.php
@@ -7,7 +7,7 @@
 
 class Evaluate_model extends CI_Model
 {
-    private $tb = '';
+    private $tb = 'offline_';
     private $db = '';
 
     public function __construct()
@@ -18,5 +18,37 @@ class Evaluate_model extends CI_Model
         }
         $this->load->config('realtime_conf.php');
         $this->load->model('waymap_model');
+    }
+
+    /**
+     * 获取路口指标排序列表
+     * @param $data['city_id']    Y 城市ID
+     * @param $data['quota_key']  Y 指标KEY
+     * @param $data['date']       N 日期 默认当前日期
+     * @param $data['time_point'] N 时间 默认当前时间
+     * @return array
+     */
+    public function getJunctionQuotaSortList($data)
+    {
+    	if (empty($data)) {
+    		return [];
+    	}
+
+    	$result = [];
+
+    	$table = $this->tb . $data['city_id'];
+    	$where = 'date = (select updated_at from ' . $table . ' order by updated_at desc limit 1)';
+
+    	$this->db->select("logic_junction_id, {$data['quota_key']}");
+    	$this->db->from($table);
+    	$this->db->where($where);
+    	$res = $this->db->get()->result_array();
+    	echo "sql = " . $this->db->last_query();
+    	if (empty($res)) {
+    		return [];
+    	}
+
+
+    	return $result;
     }
 }
