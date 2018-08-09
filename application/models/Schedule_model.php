fb00b38528e8e8d8d3df5bc67ac13fb91e2360de
niuyufu <niuyufu@didichuxing.com>
2018-08-09 15:07:38 +0800

    opt

--- application/models/Schedule_model.php
+++ application/models/Schedule_model.php
@@ -23,23 +23,29 @@ class Schedule_model extends CI_Model
     }
 
     //检查是否存在区域配时任务?
-    public function process($cityId, $traceId){
-        $params = ['city_id'=>$cityId];
-        $areaJson = httpPOST('http://10.95.100.106:8088/welcome/getAreaList', $params);
+    public function process($cityId, $traceId)
+    {
+        $params = ['city_id' => $cityId];
+        if (ENVIRONMENT != "production") {
+            $baseUri = 'http://100.90.164.31:8006/signal-mis/TimingAdaptation/getAreaList';
+        } else {
+            $baseUri = 'http://100.90.164.31:8006/signal-mis/TimingAdaptation/getAreaList';
+        }
+        $areaJson = httpPOST($baseUri, $params);
         $areaArr = json_decode($areaJson, true);
-        $areaList = !empty($areaArr["data"]["areaList"]) ? $areaArr["data"]["areaList"] : [];
-        foreach ($areaList as $var=>$val){
+        $areaList = !empty($areaArr["data"]) ? $areaArr["data"] : [];
+        foreach ($areaList as $var => $val) {
             $task = $this->db->from('schedule_task')
                 ->where('city_id', $cityId)
-                ->where('task_id', $val["area_id"])
+                ->where('task_id', $val["id"])
                 ->where('task_type', 1)
                 ->where('delete_time', "1970-01-01 00:00:00")
                 ->get()->result_array();
-            if(empty($task)){
+            if (empty($task)) {
                 //insert
                 $data = array(
                     'city_id' => $cityId,
-                    'task_id' => $val["area_id"],
+                    'task_id' => $val["id"],
                     'task_type' => 1,
                     'last_status' => 0,
                     'last_result' => '',
@@ -49,10 +55,10 @@ class Schedule_model extends CI_Model
                     'update_time' => date("Y-m-d H:i:s"),
                 );
                 $this->db->insert('schedule_task', $data);
-                echo "[INFO] " . date("Y-m-d\TH:i:s") . " trace_id=".$traceId."||area_id=".$val["area_id"]."||message=insert\n\r";
-            }else{
+                echo "[INFO] " . date("Y-m-d\TH:i:s") . " trace_id=" . $traceId . "||area_id=" . $val["id"] . "||message=insert\n\r";
+            } else {
                 //nothing
-                echo "[INFO] " . date("Y-m-d\TH:i:s") . " trace_id=".$traceId."||area_id=".$val["area_id"]."||message=nothing\n\r";
+                echo "[INFO] " . date("Y-m-d\TH:i:s") . " trace_id=" . $traceId . "||area_id=" . $val["id"] . "||message=nothing\n\r";
             }
         }
     }
