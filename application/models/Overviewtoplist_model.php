acdca2418a32f260c9fb451abcb1c3d884f63d2c
licxi <lichaoxi_i@didichuxing.com>
2018-08-14 17:13:07 +0800

    从redis里获取最新的hour

--- application/models/Overviewtoplist_model.php
+++ application/models/Overviewtoplist_model.php
@@ -83,7 +83,6 @@ class Overviewtoplist_model extends CI_Model
 
     private function getLastestHour($cityId, $date = null)
     {
-        return "15:32:00";
         if(($hour = $this->redis_model->getData("its_realtime_lasthour_$cityId"))) {
             return $hour;
         }
