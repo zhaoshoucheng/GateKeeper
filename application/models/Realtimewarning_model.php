e3eff91a406d9dbd80e14d49f185fac217a053d8
niuyufu <niuyufu@didichuxing.com>
2018-08-14 15:16:29 +0800

    opt

--- application/models/Realtimewarning_model.php
+++ application/models/Realtimewarning_model.php
@@ -131,7 +131,7 @@ class Realtimewarning_model extends CI_Model
 
         $currentId = 0;
         while (1) {
-            $sql = "SELECT * FROM `{$tableName}` WHERE `updated_at`>\"{$date}\" and hour=\"{$hour}\" and id>{$currentId} {$rtwRule['where']} order by id asc limit 1000";
+            $sql = "SELECT * FROM `{$tableName}` WHERE `updated_at`>\"{$date}\" and hour=\"{$hour}\" and id>{$currentId} {$rtwRule['where']} order by id asc limit 100";
             $query = $this->db->query($sql);
             $result = $query->result_array();
             if (empty($result)) {
