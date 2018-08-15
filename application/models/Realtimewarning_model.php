5f325c8418eef9d2844122b21eb1dae4e1e9b36c
niuyufu <niuyufu@didichuxing.com>
2018-08-15 19:21:16 +0800

    opt

--- application/models/Realtimewarning_model.php
+++ application/models/Realtimewarning_model.php
@@ -203,7 +203,6 @@ class Realtimewarning_model extends CI_Model
             $data = $this->db->select('*')
                 ->from($tableName)
                 ->where('hour', $hour)
-                ->where('traj_count >=', 10)
                 ->where('updated_at >=', $date . ' 00:00:00')
                 ->where('updated_at <=', $date . ' 23:59:59')
                 ->limit($value, $offset)
