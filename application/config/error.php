9c134af3faf81ccbdc2bc38164184b36e7c0f7e3
niuyufu <niuyufu@didichuxing.com>
2018-08-27 16:30:58 +0800

    opt

--- application/config/error.php
+++ application/config/error.php
@@ -13,6 +13,8 @@ define('ERR_DEFAULT', 100000);            // 默认
 define('ERR_PARAMETERS', 100400);         // 参数传递错误码
 define('ERR_REQUEST_TIMING_API', 200001); // 请求配时API出错
 define('ERR_REQUEST_WAYMAP_API', 200002); // 请求路网API出错
+define('ERR_UNKNOWN', 900001); // 未知错误
+
 
 /**
  * 错误码共7位
