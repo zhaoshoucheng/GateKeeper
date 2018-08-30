72fb06bdb776a1ad76d20624538faa37b3196d6c
ningxiangbing <ningxiangbing@didichuxing.com>
2018-08-30 17:53:49 +0800

    sql 防注入 系我一生心，负你千行泪

--- application/models/Road_model.php
+++ application/models/Road_model.php
@@ -79,6 +79,7 @@ class Road_model extends CI_Model
             'created_at'         => date('Y-m-d H:i:s'),
             'updated_at'         => date('Y-m-d H:i:s'),
         ];
+        $insertData = $this->db->escape($insertData);
 
         $res = $this->db->insert($this->tb, $insertData);
         if (!$res) {
