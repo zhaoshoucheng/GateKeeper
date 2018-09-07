9b215312e0b7251a41dbb3865b4ff9b18daab0aa
lizhaohua <lizhaohua@didichuxing.com>
2018-09-07 16:00:36 +0800

    fix bug

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -124,8 +124,7 @@ class Cron extends CI_Controller
 				$all = [];
 				foreach ($checkItems as $item) {
 					$data = array ('msgtype' => 'text','text' => array ('content' => json_encode($item)));
-					$data_string = json_encode($data);
-					httpPOST($webhook, $data_string);
+					$this->requestByCurl($webhook, $data);
 					if (isset($item['params']['city_id'])) {
 						$item['params']['city_id'] = $city_id;
 					}
@@ -165,8 +164,7 @@ class Cron extends CI_Controller
 				$message = $e->getMessage();
 				var_dump($message);
 				$data = array ('msgtype' => 'text','text' => array ('content' => $message));
-				$data_string = json_encode($data);
-				httpPOST($webhook, $data_string);
+				$this->requestByCurl($webhook, $data);
 				continue;
 			}
 
@@ -180,4 +178,21 @@ class Cron extends CI_Controller
 		$data = http_build_query($params);
 		return md5($method . $url . $data) . '.json';
 	}
+
+	private function requestByCurl($remote_server, $post_data) {
+		$post_string = json_encode($post_data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $remote_server);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array ('Content-Type: application/json;charset=utf-8'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // 线下环境不用开启curl证书验证, 未调通情况可尝试添加该代码
+        // curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        // curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
 }
