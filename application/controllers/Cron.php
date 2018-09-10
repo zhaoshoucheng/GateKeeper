35907473a2923f8628ba3ae3e8ccc5071f98d063
lizhaohua <lizhaohua@didichuxing.com>
2018-09-10 16:50:03 +0800

    remove requestByCurl and add httpPost

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -162,7 +162,7 @@ class Cron extends CI_Controller
 				$message = $e->getMessage();
 				var_dump($message);
 				$data = array ('msgtype' => 'text','text' => array ('content' => $message));
-				$this->requestByCurl($webhook, $data);
+				httpPOST($webhook, $data, 0, 'json');
 				continue;
 			}
 
@@ -184,21 +184,4 @@ class Cron extends CI_Controller
 		$this->load->helper('http');
 		httpPOST($webhook, $data, 0, 'json');
 	}
-
-	private function requestByCurl($remote_server, $post_data) {
-		$post_string = json_encode($post_data);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $remote_server);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array ('Content-Type: application/json;charset=utf-8'));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // 线下环境不用开启curl证书验证, 未调通情况可尝试添加该代码
-        // curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
 }
