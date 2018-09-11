52ced3c938ff67924ee98dcea48fe8731c93cfe2
lizhaohua <lizhaohua@didichuxing.com>
2018-09-11 12:12:27 +0800

    fix bug

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -324,6 +324,9 @@ class Welcome extends CI_Controller {
     }
 
     public function token() {
+    	$remote_ip = $_SERVER["REMOTE_ADDR"];
+    	$uri = $_SERVER["REQUEST_URI"];
+    	var_dump([$remote_ip, $uri]);
     	$secret = '310173de4b64b866e6f0cf4841178b84';
     	$get = $this->input->get();
     	$post = $this->input->post();
