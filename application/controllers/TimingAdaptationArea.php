9068e7d9a1e1379dc257ac7e3683e09485897d3f
ningxiangbing <ningxiangbing@didichuxing.com>
2018-09-12 11:18:46 +0800

    controller名称更新 试问闲愁都几许，一川烟草，满城风絮，梅子黄时雨

--- application/controllers/TimingAdaptationArea.php
+++ application/controllers/TimingAdaptationArea.php
@@ -7,12 +7,12 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TimingAdaptation extends MY_Controller
+class TimingAdaptationArea extends MY_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('timingadaptatin_model');
+        $this->load->model('timingadaptatinarea_model');
     }
 
     /**
---
+++ application/models/Timingadaptationarea_model.php
@@ -0,0 +1,17 @@
+<?php
+/********************************************
+ * # desc:    自适应区域模型
+ * # author:  ningxiangbing@didichuxing.com
+ * # date:    2018-07-25
+ ********************************************/
+
+class Timingadaptationarea_model extends CI_Model
+{
+    private $tb = '';
+    private $db = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
