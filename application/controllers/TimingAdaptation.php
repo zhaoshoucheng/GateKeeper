4545914b73d45edd256b56f2576626662a0f4a56
ningxiangbing <ningxiangbing@didichuxing.com>
2018-09-12 10:56:07 +0800

    获取区域列表 一日不思量，也攒眉千度

---
+++ application/controllers/TimingAdaptation.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+# 自适应
+# user:ningxiangbing@didichuxing.com
+# date:2018-09-10
+***************************************************************/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TimingAdaptation extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('timingadaptatin_model');
+    }
+
+    /**
+     * 获取自适应区域列表
+     * @param city_id    interger Y 城市ID
+     */
+    public function getAreaList()
+    {
+        $params = $this->input->post(NULL, TRUE);
+        if (intval($params['city_id']) < 1) {
+            $this->errno = ERR_PARAMETERS;
+            $this->errmsg = '参数city_id传递错误！';
+            return;
+        }
+
+        // 调用signal-mis接口
+        try {
+            $url = $this->config->item('signal_mis_interface') . '/TimingAdaptation/getAreaList';
+            $data['city_id'] = intval($params['city_id']);
+
+            $result = httpPOST($url, $data);
+            var_dump($result);
+
+        } catch (Exception $e) {
+            $this->errno = ERR_PARAMETERS;
+            $this->errmsg = '调用signal-mis的getAreaList接口出错！';
+            return;
+        }
+    }
+}
