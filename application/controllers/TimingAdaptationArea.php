68825a7e251fb14112d4c955e9ff36ed5946fd15
licxi <lichaoxi_i@didichuxing.com>
2018-09-17 15:02:24 +0800

    更新自适应路口开关

---
+++ application/controllers/TimingAdaptationArea.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************
+# 时段优化全天路口散点图类
+# user:ningxiangbing@didichuxing.com
+# date:2018-06-07
+ ***************************************************************/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TimingAdaptationArea extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 更新自适应路口开关
+     */
+    public function junctionSwitch()
+    {
+        $params = $this->input->post();
+
+        $validator = Validator::make($params, [
+            'logic_junction_id' => 'required',
+            'area_id' => 'required',
+            'is_upload' => 'required;in:0,1',
+        ]);
+
+        if($validator->fail()) {
+            $this->errno = ERR_PARAMETERS;
+            $this->errmsg = $validator->firstError();
+            return;
+        }
+
+        $address = 'http://100.90.164.31:8006/signal-mis';
+        $data = httpPOST($address . '/TimingAdaptation/junctionSwitch', $params);
+
+        $data = json_decode($data, true);
+        return $this->response($data);
+    }
+}
