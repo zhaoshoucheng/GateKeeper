09a55ac36709ec1fdbf33137af0fe1febea42185
licxi <lichaoxi_i@didichuxing.com>
2018-09-18 16:23:00 +0800

    opt

--- application/models/Timingadaptation_model.php
+++ application/models/Timingadaptation_model.php
@@ -15,9 +15,45 @@ class Timingadaptation_model extends CI_Model
         $adapt = $this->getAdaptInfo($logic_junction_id);
         $current = $this->getCurrentInfo($logic_junction_id);
 
-        $data = $this->arrayMergeRecursive(json_decode($adapt['timing_info'], true)['data'] ?? [], $current);
+        $tod_movement = Collection::make($current['tod'] ?? [])
+            ->column(null, 'tod_id')->map(function ($tod) {
+                return Collection::make($tod['movement_timing'] ?? [])
+                    ->column(null, 'movement_id')->map(function ($movement) {
+                        return Collection::make($movement['timing'] ?? [])
+                            ->column('state')->get();
+                    })->get();
+            });
 
-        return $data;
+        return $tod_movement;
+//
+//        $data['tod'] = array_map(function ($tod) use ($tod_movement) {
+//            return [
+//                'plan_id' => $tod['plan_id'],
+//                'extra_time' => $tod['extra_time'],
+//                'movement_timing' => array_map(function ($movement) use ($tod, $tod_movement)  {
+//                    return [
+//                        'movement_id' => $movement['movement_id'] ?? '',
+//                        'channel' => $movement['channel'] ?? '',
+//                        'phase_id' => $movement['phase_id'] ?? '',
+//                        'phase_seq' => $movement['phase_seq'] ?? '',
+//                        'yellow' => $tod_movement[$tod['plan_id']][$movement['movement_id']][2]['duration'] ?? '',
+//                        'timing' => [
+//                            'start_time' => $tod_movement[$tod['plan_id']][$movement['movement_id']][1]['start_time'] ?? '',
+//                            'suggest_start_time' => $movement['timing'][1]['start_time'] ?? '',
+//                            'duration' => $tod_movement[$tod['plan_id']][$movement['movement_id']][1]['duration'] ?? '',
+//                            'suggest_duration' => $movement['timing'][1]['duration'] ?? '',
+//                            'max' => $tod_movement[$tod['plan_id']][$movement['movement_id']][1]['max'] ?? '',
+//                            'suggest_max' => $movement['timing'][1]['max'] ?? '',
+//                            'min' => $tod_movement[$tod['plan_id']][$movement['movement_id']][1]['min'] ?? '',
+//                            'suggest_min' => $movement['timing'][1]['min'] ?? '',
+//                        ],
+//                        'flow' => $movement['flow'],
+//                    ];
+//                }, $tod['movement_timing']),
+//            ];
+//        }, $data['tod'] ?? []);
+
+
     }
 
     /**
