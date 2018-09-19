07664f0c7a245e1c4bc5a24132247c865349ef1f
ningxiangbing <ningxiangbing@didichuxing.com>
2018-09-19 17:40:45 +0800

    严谨参数校验 @ 泠泠七弦上，静听松风寒。 古调虽自爱，今人多不弹。

--- application/controllers/TimingAdaptationArea.php
+++ application/controllers/TimingAdaptationArea.php
@@ -407,7 +407,7 @@ class TimingAdaptationArea extends MY_Controller
             return;
         }
 
-        if (empty($params['quota_key'])) {
+        if (empty($params['quota_key']) || !in_array(trim($params['quota_key']), ['speed', 'stopDelay'], true)) {
             $this->errno = ERR_PARAMETERS;
             $this->errmsg = '参数quota_key传递错误！';
             return;
