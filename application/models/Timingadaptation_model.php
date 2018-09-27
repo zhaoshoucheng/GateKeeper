a21e0e0562219d4dc8ada7407a3dcf46a0ff0f70
licxi <lichaoxi_i@didichuxing.com>
2018-09-27 16:19:04 +0800

    fix bug

--- application/models/Timingadaptation_model.php
+++ application/models/Timingadaptation_model.php
@@ -159,6 +159,11 @@ class Timingadaptation_model extends CI_Model
             $flowIds = array_column(array_column($tod['movement_timing'], 'flow'), 'logic_flow_id');
             $flows = $this->getTwiceStopRate($flowIds, $params);
             foreach ($tod['movement_timing'] as $mk => &$movement) {
+                if($movement['flow']['logic_flow_id'] == '') {
+                    unset($tod['movement_timing'][$mk]);
+                    continue;
+                }
+
                 $movement['flow']['twice_stop_rate'] = $flows[$movement['flow']['logic_flow_id']] ?? '';
                 $greenCurrents = Collection::make($current['tod'][$tk]['movement_timing'][$mk]['timing'] ?? [])->where('state', 1)->get();
                 $greens = $this->arrayMergeRecursive($movement['timing'], $greenCurrents);
