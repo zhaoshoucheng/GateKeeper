b595b41dbca891c61449e52d3860bf89313600fe
niuyufu <niuyufu@didichuxing.com>
2018-10-09 19:01:41 +0800

    调整代理请求信息

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -37,9 +37,9 @@ class Welcome extends CI_Controller {
 	public function test(){
 	    $queryStr="x0=114.48136&y0=38.03515&x1=114.48492&y1=38.03534&st=1539046794&et=1539048594&userid=signal&samples=-1&biztypes=all&index=all&driverlist=all&status=5&app_id=xmmtrace&sign=1111";
         $queryMap = [];
-        parse_str($queryStr, &$queryMap);
+        parse_str($queryStr, $queryMap);
         print_r($queryMap);exit;
-    
+
 
         $this->load->model('redis_model');
         $this->redis_model->setEx("11222", "hello", 24*3600);
