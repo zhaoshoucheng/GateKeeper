b3a67aa0d365c2fb0bc7ad1c9429093be2194e76
niuyufu <niuyufu@didichuxing.com>
2018-10-09 16:48:26 +0800

    添加auth_whiteip

--- application/config/appkey.php
+++ application/config/appkey.php
@@ -45,4 +45,16 @@ $config['authirized_apps'] = array(
         'white_ips' => array(
         )
     ),
+    '1004' => array(
+        'name'  => 'token',
+        'secret'=> '3a01e6c56bcce94ee5de073df3d512d2',
+        'open_api'   => array(
+            'AdaptMovement/queryconf',
+            'AdaptMovement/updateconf',
+            'AdaptMovement/deleteconf',
+        ),
+        'white_ips' => array(
+            "172.25.32.53"
+        )
+    ),
 );
