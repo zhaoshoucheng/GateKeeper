e6ea2e152a1a308812845d81bb89db754c7d6360
licxi <lichaoxi_i@didichuxing.com>
2018-10-10 15:40:41 +0800

    完善数据逻辑

--- application/config/permission/menu_conf.php
+++ application/config/permission/menu_conf.php
@@ -6,7 +6,13 @@ $config['menu'] = [
 
     'check' => function ($user) {
         $users = [
-
+            '18611198971',
+            '13718307981',
+            '13810015784',
+            '13261208949',
+            '13810530502',
+            '18612588551',
+            '13911697834'
         ];
 
         if(in_array($user, $users)) {
@@ -93,6 +99,46 @@ $config['menu'] = [
                 ],
         ],
         // 北京区域受限菜单
-        2 => []
+        2 => [
+            [
+                'name' => '概览',
+                'url' => '#/overview',
+                'remark ' => 'signal',
+            ],
+            [
+                'name' => '诊断',
+                'url' => '/signalpro/#/diagnose/',
+                'remark ' => 'signal',
+                'son' => [
+                    [
+                        'name' => '趋势',
+                        'url' => 'tendency',
+                    ],
+                    [
+                        'name' => '详情',
+                        'url' => 'index',
+                    ],
+                ],
+            ],
+            [
+                'name' => '优化',
+                'url' => '/signalpro/#/optimize/',
+                'remark ' => 'signal',
+                'son' => [
+                    [
+                        'name' => '单点时段优化',
+                        'url' => 'signal',
+                    ],
+                    [
+                        'name' => '单点绿信比优化',
+                        'url' => 'green',
+                    ],
+                    [
+                        'name' => '干线协调优化',
+                        'url' => 'coordinate',
+                    ],
+                ],
+            ],
+        ]
     ],
 ];
