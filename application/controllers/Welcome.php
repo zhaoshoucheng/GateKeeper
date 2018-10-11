b3973894ce04309804ee02bfb9e638e5051d9013
niuyufu <niuyufu@didichuxing.com>
2018-10-11 19:24:20 +0800

    兼容修复

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -40,11 +40,11 @@ class Welcome extends CI_Controller {
         parse_str($queryStr, $queryMap);
         $sign = getSign($queryMap, "3a01e6c56bcce94ee5de073df3d512d2");
 
-        $url="http://100.90.164.31:8088/AdaptMovement/queryconf?".$queryStr."&sign=".$sign;
+        $url="http://100.90.164.31:8082/AdaptMovement/queryconf?".$queryStr."&sign=".$sign;
         echo $sign;
         echo "<br/>";
         echo $url;exit;
-        
+
 
 	    $queryStr="x0=114.48136&y0=38.03515&x1=114.48492&y1=38.03534&st=1539046794&et=1539048594&userid=signal&samples=-1&biztypes=all&index=all&driverlist=all&status=5&app_id=xmmtrace&ts=".time();
         $queryMap = [];
