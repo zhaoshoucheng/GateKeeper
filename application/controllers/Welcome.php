64be463f1e3f9306bfac496e00beaae8b84d8a8c
niuyufu <niuyufu@didichuxing.com>
2018-10-19 15:09:47 +0800

    调整参数

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -55,6 +55,17 @@ class Welcome extends CI_Controller {
 //        echo "<br/>";
 //        echo $url;exit;
 
+        $queryStr="send_type=1&tos=niuyufu@didichuxing.com&subject=1122&content=1&log_id=1&ts=".time();
+        $queryMap = [];
+        parse_str($queryStr, $queryMap);
+        $sign = getSign($queryMap, "3a01e6c56bcce94ee5de073df3d512d5");
+
+        $url="http://100.90.164.31:8082/signalpro/api/warning/notify?".$queryStr."&sign=".$sign;
+        echo $sign;
+        echo "<br/>";
+        echo $url;exit;
+
+
 	    $queryStr="x0=114.48136&y0=38.03515&x1=114.48492&y1=38.03534&st=1539046794&et=1539048594&userid=signal&samples=-1&biztypes=all&index=all&driverlist=all&status=5&app_id=xmmtrace&ts=".time();
         $queryMap = [];
         parse_str($queryStr, $queryMap);
