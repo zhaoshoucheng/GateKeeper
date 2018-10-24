f00ba85f7d2353055c672160be0876d00ae093ba
licxi <lichaoxi_i@didichuxing.com>
2018-10-24 11:32:02 +0800

    格式化CURL，修复 dateRange 辅助函数参数BUG

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -3,8 +3,10 @@
 if (!function_exists('checkSign')) {
     /**
      * 验证签名
+     *
      * @param $params
      * @param $secret
+     *
      * @return bool
      */
     function checkSign($params, $secret)
@@ -15,8 +17,8 @@ if (!function_exists('checkSign')) {
         }
 
         //拦截ddmp附加信息
-        foreach ($params as $pKey=>$pItem){
-            if(strpos($pKey,"carrera")===0){
+        foreach ($params as $pKey => $pItem) {
+            if (strpos($pKey, "carrera") === 0) {
                 unset($params[$pKey]);
             }
         }
@@ -30,8 +32,9 @@ if (!function_exists('checkSign')) {
 
 if (!function_exists('getSignature')) {
     /**
-     * @param array $params API调用的请求参数集合的关联数组，不包含sign参数
+     * @param array  $params API调用的请求参数集合的关联数组，不包含sign参数
      * @param string $secret 申请到的秘钥 123456
+     *
      * @return string
      */
     function getSignature($params, $secret)
@@ -53,8 +56,9 @@ if (!function_exists('getSignature')) {
 
 if (!function_exists('getSign')) {
     /**
-     * @param array $params API调用的请求参数集合的关联数组，不包含sign参数
+     * @param array  $params API调用的请求参数集合的关联数组，不包含sign参数
      * @param string $secret 申请到的app_id对应秘钥
+     *
      * @return string
      */
     function getSign($params, $secret)
@@ -67,7 +71,7 @@ if (!function_exists('getSign')) {
         //遍历排序后的参数数组中的每一个key/value对
         foreach ($params as $k => $v) {
             //为key/value对生成一个key=value格式的字符串，并拼接到待签名字符串后面
-            $str .= "$k=" . urldecode($v)."&";
+            $str .= "$k=" . urldecode($v) . "&";
         }
 
         //将签名密钥拼接到签名字符串最后面
@@ -85,13 +89,14 @@ if (!function_exists('des_encrypt')) {
      *
      * @param $str
      * @param $key
+     *
      * @return mixed|string
      */
     function des_encrypt($str, $key)
     {
         $block = @mcrypt_get_block_size('des', 'ecb');
-        $pad = $block - (strlen($str) % $block);
-        $str .= str_repeat(chr($pad), $pad);
+        $pad   = $block - (strlen($str) % $block);
+        $str   .= str_repeat(chr($pad), $pad);
         return safe_b64encode(@mcrypt_encrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB));
     }
 }
@@ -102,6 +107,7 @@ if (!function_exists('des_decrypt')) {
      *
      * @param $str
      * @param $key
+     *
      * @return bool|string
      */
     function des_decrypt($str, $key)
@@ -120,12 +126,13 @@ if (!function_exists('safe_b64encode')) {
      * 处理特殊字符
      *
      * @param $string
+     *
      * @return mixed|string
      */
     function safe_b64encode($string)
     {
         $data = base64_encode($string);
-        $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
+        $data = str_replace(['+', '/', '='], ['-', '_', ''], $data);
         return $data;
     }
 }
@@ -135,11 +142,12 @@ if (!function_exists('safe_b64decode')) {
      * 解析特殊字符
      *
      * @param $string
+     *
      * @return bool|string
      */
     function safe_b64decode($string)
     {
-        $data = str_replace(array('-', '_'), array('+', '/'), $string);
+        $data = str_replace(['-', '_'], ['+', '/'], $string);
         $mod4 = strlen($data) % 4;
         if ($mod4) {
             $data .= substr('====', $mod4);
@@ -153,6 +161,7 @@ if (!function_exists('isInt')) {
      * 验证字符串是否为数字
      *
      * @param $number string
+     *
      * @return bool
      */
     function isInt($number)
@@ -164,32 +173,39 @@ if (!function_exists('isInt')) {
     }
 }
 
-if(!function_exists('dateRange')) {
+if (!function_exists('dateRange')) {
     /**
      * 获取指定时间范围内的粒度为 天 的倍数的 指定格式 的数据集合
      *
-     * @param $start
-     * @param $end
-     * @param int $skip
+     * @param        $start
+     * @param        $end
+     * @param int    $skip
      * @param string $format
+     *
      * @return array
+     * @throws Exception
      */
     function dateRange($start, $end, $skip = 1, $format = 'Y-m-d')
     {
+        if ($start <= $end) {
+            throw new Exception('起始时间必须小于等于结束时间');
+        }
+
         return array_map(function ($item) use ($format) {
             return date($format, $item);
         }, range(strtotime($start), strtotime($end), $skip * 60 * 60 * 24));
     }
 }
 
-if(!function_exists('hourRange')) {
+if (!function_exists('hourRange')) {
     /**
      * 获取指定时间范围内的 粒度为 分钟 的 指定格式的数据集合
      *
      * @param string $start
      * @param string $end
-     * @param int $skip
+     * @param int    $skip
      * @param string $format
+     *
      * @return array
      */
     function hourRange($start = '00:00', $end = '23:30', $skip = 30, $format = 'H:i')
@@ -200,11 +216,13 @@ if(!function_exists('hourRange')) {
     }
 }
 
-if(!function_exists('arrayMergeRecursive')) {
+if (!function_exists('arrayMergeRecursive')) {
     /**
      * 递归合并数组（支持数字键数组）
+     *
      * @param $target
      * @param $source
+     *
      * @return mixed
      */
     function arrayMergeRecursive($target, $source)
@@ -215,10 +233,10 @@ if(!function_exists('arrayMergeRecursive')) {
         $keys = array_unique(array_merge($tKeys, $sKeys));
 
         foreach ($keys as $key) {
-            if(array_key_exists($key, $source) && !array_key_exists($key, $target)) {
+            if (array_key_exists($key, $source) && !array_key_exists($key, $target)) {
                 $target[$key] = $source[$key];
             } elseif (array_key_exists($key, $source) && array_key_exists($key, $target)) {
-                if(is_array($target[$key]) && is_array($source[$key])) {
+                if (is_array($target[$key]) && is_array($source[$key])) {
                     $target[$key] = arrayMergeRecursive($target[$key], $source[$key]);
                 } else {
                     $target[$key] = [$target[$key], $source[$key]];
@@ -229,23 +247,24 @@ if(!function_exists('arrayMergeRecursive')) {
     }
 }
 
-if(!function_exists('snakeCompact')) {
+if (!function_exists('snakeCompact')) {
     /**
      * 类似于 compact ，将传入变量名的变量转换为数组，数组的键由驼峰式转换为下划线
      *
      * @param mixed ...$var
+     *
      * @return array
      */
     function snakeCompact(...$var)
     {
-        if(empty($var)) {
+        if (empty($var)) {
             return [];
         }
 
         $res = [];
         foreach ($var as $item) {
-            $name =  preg_replace_callback('/([A-Z]{1})/',function($matches){
-                return '_'.strtolower($matches[0]);
+            $name       = preg_replace_callback('/([A-Z]{1})/', function ($matches) {
+                return '_' . strtolower($matches[0]);
             }, $item);
             $res[$name] = $GLOBALS[$item];
         }
@@ -253,11 +272,11 @@ if(!function_exists('snakeCompact')) {
     }
 }
 
-if(!function_exists('snakeExtract')) {
+if (!function_exists('snakeExtract')) {
     function snakeExtract(array $arr)
     {
         foreach ($arr as $key => $value) {
-            $name = preg_replace_callback('/([-_]+([a-z]{1}))/i',function($matches){
+            $name           = preg_replace_callback('/([-_]+([a-z]{1}))/i', function ($matches) {
                 return strtoupper($matches[2]);
             }, $key);
             $GLOBALS[$name] = $value;
@@ -265,7 +284,7 @@ if(!function_exists('snakeExtract')) {
     }
 }
 
-if(!function_exists('getExcelArray')) {
+if (!function_exists('getExcelArray')) {
     function getExcelArray($data)
     {
         $timeArray = hourRange();
@@ -280,7 +299,7 @@ if(!function_exists('getExcelArray')) {
         }, $data);
 
         foreach ($data as $key => $value) {
-            $column = [];
+            $column   = [];
             $column[] = $key;
             foreach ($timeArray as $item) {
                 $column[] = $value[$item] ?? '-';
@@ -292,8 +311,9 @@ if(!function_exists('getExcelArray')) {
     }
 }
 
-if(!function_exists('intToChr')) {
-    function intToChr($index, $start = 65) {
+if (!function_exists('intToChr')) {
+    function intToChr($index, $start = 65)
+    {
         $str = '';
         if (floor($index / 26) > 0) {
             $str .= $this->intToChr(floor($index / 26) - 1);
