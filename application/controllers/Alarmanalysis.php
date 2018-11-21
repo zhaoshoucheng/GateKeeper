94cea8b95a9f6a01b7be151da3cc3a9b9b5022d2
ningxiangbing <ningxiangbing@didichuxing.com>
2018-11-21 15:19:22 +0800

    开发城市/路口报警时段分布接口ing.    @    千里共如何，微风吹兰杜。

--- application/controllers/Alarmanalysis.php
+++ application/controllers/Alarmanalysis.php
@@ -57,16 +57,29 @@ class Alarmanalysis extends MY_Controller
      * @param $params['city_id']           int    Y 城市ID
      * @param $params['logic_junction_id'] string N 逻辑路口ID 当：不为空时，按路口查询;为空时，按城市查询
      * @param $params['frequency_type']    int    Y 频率类型 0：全部 1：常发 2：偶发
-     * @param $params['time_range']        string Y 查询时间段 例：当前时间为：2018-11-13
-     *                                                           当天：2018-11-13-2018-11-13
-     *                                                           七日：2018-11-06-2018-11-12
-     *                                                           30日：同七日
-     *                                                           自定义：同七日
+     * @param $params['start_time']        string Y 查询开始日期 yyyy-mm-dd
+     * @param $params['end_time']          string Y 查询结束日期 yyyy-mm-dd 开始日期与结束日期一致认为查询当天从0点到当前整点的数据
      * @return json
      */
     public function alarmTimeDistribution()
     {
         $params = $this->input->post(null, true);
+
+        // 校验参数
+        $this->validate([
+            'frequency_type' => 'required|in_list[' . implode(',', array_keys($this->config->item('frequency_type'))) . ']',
+            'start_time'     => 'required|trim|regex_match[/\d{4}-\d{2}-\d{2}/]',
+            'end_time'       => 'required|trim|regex_match[/\d{4}-\d{2}-\d{2}/]',
+            'city_id'        => 'required|is_natural_no_zero',
+        ]);
+
+        $params['logic_junction_id'] = !empty($params['logic_junction_id'])
+                                        ? strip_tags(trim($params['logic_junction_id']))
+                                        : '';
+
+        $result = $this->alarmanalysisService->alarmTimeDistribution($params);
+
+        $this->response($result);
     }
 
     /**
--- application/services/AlarmanalysisService.php
+++ application/services/AlarmanalysisService.php
@@ -55,15 +55,23 @@ class AlarmanalysisService extends BaseService
     private function getDailyAlarmAnalysis($params)
     {
         // 组织DSL所需json
-        $json = '{"from":0,"size":0,"query":{"bool":{"must":{"bool":{"must":[{"match":{"city_id":{"query":'.$params['city_id'].',"type":"phrase"}}},{"match":{"date":{"query":"'.$params['start_time'].'","type":"phrase"}}}';
+        $json = '{"from":0,"size":0,"query":{"bool":{"must":{"bool":{"must":[';
+
+        // where city_id
+        $json .= '{"match":{"city_id":{"query":'.$params['city_id'].',"type":"phrase"}}}';
+
+        // where date
+        $json .= ',{"match":{"date":{"query":"'.$params['start_time'].'","type":"phrase"}}}';
 
         if (!empty($params['logic_junction_id'])) { // 单路口报警分析查询
+            // where logic_junction_id
             $json .= ',{"match":{"logic_junction_id":{"query":"'.$params['logic_junction_id'].'","type":"phrase"}}}';
         }
 
         // 当选择了报警频率时
         if ($params['frequency_type'] != 0
             && array_key_exists($params['frequency_type'], $this->config->item('frequency_type'))) {
+            // where frequency_type
             $json .= ',{"match":{"frequency_type":{"query":'. $params['frequency_type'] .',"type":"phrase"}}}';
         }
 
@@ -193,5 +201,89 @@ class AlarmanalysisService extends BaseService
         $resultData['dataList'] = array_merge($continuousTime, $temp);
 
         return $resultData;
+    }
+
+    /**
+     * 城市/路口报警时段分布接口
+     * @param $params['city_id']           int    Y 城市ID
+     * @param $params['logic_junction_id'] string N 逻辑路口ID 当：不为空时，按路口查询;为空时，按城市查询
+     * @param $params['frequency_type']    int    Y 频率类型 0：全部 1：常发 2：偶发
+     * @param $params['start_time']        string Y 查询开始日期 yyyy-mm-dd
+     * @param $params['end_time']          string Y 查询结束日期 yyyy-mm-dd 开始日期与结束日期一致认为查询当天从0点到当前整点的数据
+     * @return array
+     */
+    public function alarmTimeDistribution($params)
+    {
+        if (empty($params)) {
+            return (object)[];
+        }
+
+        // 组织DSL所需json
+        $json = '{"from":0,"size":0,"query":{"bool":{"must":{"bool":{"must":[';
+
+        // where city_id
+        $json .= '{"match":{"city_id":{"query":' . $params['city_id'] . ',"type":"phrase"}}}';
+
+        if ($params['start_time'] == $params['end_time']) { // 当天
+            // where date
+            $json .= ',{"match":{"date":{"query":"' . $params['start_time'] . '","type":"phrase"}}}';
+        } else { // 多天
+            // where date
+            $json .= ',{"range":{"date":{"from":"' . $params['start_time'] . '","to":null,"include_lower":true,"include_upper":true}}}';
+            $json .= ',{"range":{"date":{"from":null,"to":"' . $params['end_time'] . '","include_lower":true,"include_upper":true}}}';
+        }
+
+        // 按路口查询
+        if (!empty($params['logic_junction_id'])) {
+            $json .= ',{"match":{"logic_junction_id":{"query":"' . $params['logic_junction_id'] . '","type":"phrase"}}}';
+        }
+
+        // 当选择了报警频率时
+        if ($params['frequency_type'] != 0
+            && array_key_exists($params['frequency_type'], $this->config->item('frequency_type'))) {
+            // where frequency_type
+            $json .= ',{"match":{"frequency_type":{"query":'. $params['frequency_type'] .',"type":"phrase"}}}';
+        }
+
+        $json .= ']}}}},"_source":{"includes":["COUNT","hour"],"excludes":[]},"fields":"hour","aggregations":{"hour":{"terms":{"field":"hour","size":200},"aggregations":{"num":{"value_count":{"field":"id"}}}}}}';
+
+        $result = $this->alarmanalysis_model->search($json);
+        if (!$result) {
+            return (object)[];
+        }
+
+        /* 处理数据 */
+        $tempRes = [];
+
+        if (!empty($result['aggregations']['hour']['buckets'])) {
+            // 路口报警类型配置
+            $junctionAlarmType = $this->config->item('junction_alarm_type');
+
+            $tempRes = array_map(function($item) use ($junctionAlarmType) {
+                return [
+                    'intHour' => $item['key'],
+                    'hour'    => $item['key'] . ':00',
+                    'value'   => $item['num']['value'],
+                ];
+            }, $result['aggregations']['date']['buckets']);
+        }
+
+        print_r($tempRes);
+
+        /* 0-23整点小时保持连续 原因：数据表中可以会有某个整点没有报警，这样会导致前端画表时出现异常 */
+        // 当前整点
+        $nowHour = date('H');
+        for ($i = 0; $i < $nowHour; $i++) {
+            $continuousHour[$i . ':00'] = [];
+        }
+
+        // 平铺数组
+        $temp = Collection::make($tempRes)->collapse()->get();
+        // 合并数组
+        $resultData['dataList'] = array_merge($continuousHour, $temp);
+
+        return $resultData;
+
+
     }
 }
