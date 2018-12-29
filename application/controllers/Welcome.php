e80e0c230aaf52ed9568dc3f8c2afd1e0340f63d
niuyufu <niuyufu@didichuxing.com>
2018-12-29 16:25:13 +0800

    添加测试方法

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -40,6 +40,12 @@ class Welcome extends CI_Controller
         $this->load->view('welcome_message');
     }
 
+    public function gateway_test()
+    {
+        $jsonStr='{"errno":0,"errmsg":"","data":{}}';
+        echo $jsonStr;
+        exit; 
+    }
 
     public function estest()
     {
