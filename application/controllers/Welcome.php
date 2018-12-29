e8b9fb3d9510e1c3b00b7bd634076876bb5b5259
niuyufu <niuyufu@didichuxing.com>
2018-12-29 16:27:02 +0800

    添加测试方法

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -12,7 +12,7 @@ class Welcome extends CI_Controller
         parent::__construct();
         $this->load->helper('http');
         $this->load->model('task_model');
-        $this->load->model('overview_model');
+        // $this->load->model('overview_model');
         $this->load->model('realtime_model');
     }
 
