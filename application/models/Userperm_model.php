a1f4d3cf3cd38934ed2e4c15e3dcdc38c1438ba2
niuyufu <niuyufu@didichuxing.com>
2019-01-04 19:05:22 +0800

    处理冲突文件

--- application/models/Userperm_model.php
+++ application/models/Userperm_model.php
@@ -32,7 +32,7 @@ class Userperm_model extends CI_Model
      */
     public function getUserPermAllGroupid()
     {
-        $key = $this->redisPrefix . "_usergroup_app";
+        $key = $this->redisPrefix . "usergroup_app";
         echo $key;
         if (!($data = $this->redis_model->getData($key))) {
             com_log_warning('_itstool_' . __CLASS__ . '_' . __FUNCTION__ . '_error', 0, "redis_get_error", compact("key"));
