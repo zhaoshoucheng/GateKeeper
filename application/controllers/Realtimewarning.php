a608f6fdadd788e5eef2fff5fa1cf9963a0ac129
niuyufu <niuyufu@didichuxing.com>
2019-01-10 17:10:40 +0800

    删除临时附加es_alarm地址

--- application/controllers/Realtimewarning.php
+++ application/controllers/Realtimewarning.php
@@ -166,7 +166,7 @@ class Realtimewarning extends Inroute_Controller
         }
 
         //临时附加线上地址
-        httpGET("http://10.85.128.81:30101/task_handler", $params, 600000);
+        //httpGET("http://10.85.128.81:30101/task_handler", $params, 600000);
 
         echo "[INFO] " . date("Y-m-d\TH:i:s") . " city_id=" . $cityId . "||hour=" . $hour . "||date=" . $date . "||trace_id=" . $traceId . "||message=task_handler done\n\r";
         // echo "[INFO] " . date("Y-m-d\TH:i:s") . " city_id=" . $cityId . "||hour=" . $hour . "||date=" . $date . "||trace_id=" . $traceId . "||message=processing\n\r";
