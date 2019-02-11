b27e284a101c38e6f2fd71f96aa4653d1b7e1717
xuqiqi <xuqiqi@didiglobal.com>
2019-02-11 18:26:31 +0800

    fix bug

--- application/services/FeedbackService.php
+++ application/services/FeedbackService.php
@@ -59,14 +59,14 @@ class FeedbackService extends BaseService
             throw new \Exception('优化方案反馈入库失败', ERR_DATABASE);
         }
 
-        $arr = ['content'=>"@18562830658 有反馈"];
-        $result = [
-            'msgtype'=>'text',
-            'text'=>$arr
-        ];
+        $data = [];
+        $data['msgtype'] = 'text';
+        $data['text']['content'] = '有反馈';
+        $data['at']['atMobiles'] = ["18562830658"];
+        $data['at']['isAtAll'] = false;
 
         $url = "https://oapi.dingtalk.com/robot/send?access_token=f9947bd6e25c7ee0264108e242999a89d425e347eaea257e9e99405c54cab97f";
-        httpPOST($url, $result,0,'json');
+        httpPOST($url, $data,0,'json');
         return [];
     }
 
@@ -84,19 +84,6 @@ class FeedbackService extends BaseService
         if (isset($params['desc'])) {
             unset($params['desc']);
         }
-
-        $arr = array(
-            'content'=>"@18562830658 有反馈"
-        );
-
-        $result = array(
-            'msgtype'=>'text',
-            'text'=>$arr
-        );
-        $data = json_encode($result,JSON_UNESCAPED_UNICODE);
-
-        $url = "https://oapi.dingtalk.com/robot/send?access_token=f9947bd6e25c7ee0264108e242999a89d425e347eaea257e9e99405c54cab97f";
-        httpPOST($url, $data,0,'json');
 
         $res = $this->feedback_model->insertFeedback($params);
 
