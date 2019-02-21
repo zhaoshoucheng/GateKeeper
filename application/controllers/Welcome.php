dfa1780ed829551672a613a34e5adeeeb118adad
niuyufu <niuyufu@didichuxing.com>
2019-02-21 19:28:33 +0800

    修复冲突代码

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -55,13 +55,10 @@ class Welcome extends CI_Controller
             $userPerm = $this->userPerm;
             if(!empty($userPerm["data"])) {
                 foreach ($userPerm["data"] as $cityId => $cityPerm) {
-<<<<<<< HEAD
                     $userPerm["data"][$cityId]["junction_num"] = count(explode(";", $cityPerm["junction_id"]));
-=======
                     if (!empty($userPerm["data"][$cityId]["junction_id"])) {
                         $userPerm["data"][$cityId]["junction_num"] = count(explode(";", $cityPerm["junction_id"]));
                     }
->>>>>>> develop
                 }
             }
             print_r($userPerm);
