6381a1b7499c2bb1dedd5365e460201afe908d55
niuyufu <niuyufu@didichuxing.com>
2019-03-01 20:03:44 +0800

    用盖的方式确保首页指标一致性

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -39,7 +39,6 @@ class MY_Controller extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-
         $this->benchmark->mark('api_start');
 
         date_default_timezone_set('Asia/Shanghai');
