ae6d6e070abb514f09bf470eee7e4028d9b87181
niuyufu <niuyufu@didichuxing.com>
2019-03-19 17:08:38 +0800

    临时修改线下es地址

--- application/config/nconf.php
+++ application/config/nconf.php
@@ -147,7 +147,7 @@ if ($development == 2) {
     $es_ext    = '';
 
     // new es
-    $quota_v2_es_server = '10.89.236.25';
+    $quota_v2_es_server = '10.89.234.61';
     $quota_v2_es_port   = '8090';
     $quota_v2_es_ext    = '';
 
