038836992433459f52b8b7c1600cda3ed8fea3ff
lizhaohua <lizhaohua@didichuxing.com>
2019-05-08 14:50:41 +0800

    [new] adp getlist

---
+++ application/models/AdpArea_model.php
@@ -0,0 +1,269 @@
+<?php
+
+/********************************************
+ * # desc:    区域数据模型
+ * # author:  niuyufu@didichuxing.com
+ * # date:    2018-08-23
+ ********************************************/
+use Didi\Cloud\Collection\Collection;
+
+class AdpArea_model extends CI_Model
+{
+    private $tb = 'area';
+
+    /**
+     * @var \CI_DB_query_builder
+     */
+    protected $db;
+
+    /**
+     * Area_model constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->signalcontrol = $this->load->database('signal_control', true);
+
+        $isExisted = $this->db->table_exists($this->tb);
+
+        if (!$isExisted) {
+            throw new \Exception('数据表不存在', ERR_DATABASE);
+        }
+    }
+
+    /**
+     * 根据城市ID获取区域列表
+     *
+     * @param $cityId
+     * @param string $select
+     * @return array
+     */
+    public function getAreasByCityId($cityId, $select = '*')
+    {
+        $res = $this->signalcontrol->select($select)
+            ->from($this->tb)
+            ->where('city_id', $cityId)
+            ->order_by('id', 'DESC')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->result_array() : $res;
+    }
+
+    /**
+     * 根据区域ID获取区域信息
+     *
+     * @param $areaId
+     * @param string $select
+     * @return mixed
+     */
+    public function getAreaByAreaId($areaId, $select = '*')
+    {
+        $res = $this->db->select($select)
+            ->from($this->tb)
+            ->where('id', $areaId)
+            ->where('delete_at', '1970-01-01')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->row_array() : $res;
+    }
+
+
+    /**
+     * 根据区域ID获取路口列表
+     *
+     * @param $areaId
+     * @param string $select
+     * @return array
+     */
+    public function getJunctionsByAreaId($areaId, $select = '*')
+    {
+        $res = $this->db->select($select)
+            ->from('area_junction_relation')
+            ->where('area_id', $areaId)
+            ->where('delete_at', '1970-01-01 00:00:00')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->result_array() : $res;
+    }
+
+    /**
+     * 根据日期，路口ID，时间，城市ID获取路口的分组信息
+     *
+     * @param $dates
+     * @param $junctionIds
+     * @param $hours
+     * @param $cityId
+     * @param $select
+     * @return Collection|null
+     */
+    public function getJunctionByCityId($dates, $junctionIds, $hours, $cityId, $select)
+    {
+        $res = $this->db->select($select)
+            ->from('junction_hour_report')
+            ->where_in('date', $dates)
+            ->where_in('logic_junction_id', $junctionIds)
+            ->where_in('hour', $hours)
+            ->where('city_id', $cityId)
+            ->group_by('date, hour')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->result_array() : $res;
+    }
+
+    /**
+     * 创建区域
+     *
+     * @param $data
+     * @return mixed 插入的ID
+     */
+    public function insertArea($data)
+    {
+        $data['create_at'] = $data['create_at'] ?? date('Y-m-d H:i:s');
+        $data['update_at'] = $data['update_at'] ?? date('Y-m-d H:i:s');
+
+        $this->db->insert($this->tb, $data);
+
+        return $this->db->insert_id();
+    }
+
+    /**
+     * 创建区域路口关联
+     *
+     * @param $areaId
+     * @param $junctionIds
+     * @return mixed
+     */
+    public function insertAreaJunctions($areaId, $junctionIds)
+    {
+        $data = array_map(function ($junctionId) use ($areaId) {
+            return [
+                'junction_id' => $junctionId,
+                'area_id' => $areaId
+            ];
+        }, $junctionIds);
+
+        return $this->insertBatchAreaJunctions($data);
+    }
+
+    /**
+     * 批量插入区域路口关联
+     *
+     * @param $data
+     * @return mixed 操作的记录条数
+     */
+    public function insertBatchAreaJunctions($data)
+    {
+        if(empty($data)) {
+            return null;
+        }
+
+        $data = array_map(function ($item) {
+            $item['create_at'] = $item['create_at'] ?? date('Y-m-d H:i:s');
+            $item['update_at'] = $item['update_at'] ?? date('Y-m-d H:i:s');
+            return $item;
+        }, $data);
+
+        return $this->db->insert_batch('area_junction_relation', $data);
+    }
+
+    /**
+     * 更新区域
+     *
+     * @param $areaId
+     * @param $data
+     * @return bool 更新的结果
+     */
+    public function updateArea($areaId, $data)
+    {
+        $data['update_at'] = $data['update_at'] ?? date('Y-m-d H:i:s');
+
+        return $this->db->where('id', $areaId)
+            ->where('delete_at', '1970-01-01 00:00:00')
+            ->update($this->tb, $data);
+    }
+
+    /**
+     * 获取区域路口关联
+     *
+     * @param $areaId
+     * @param string $select
+     * @return mixed
+     */
+    public function getAreaJunctions($areaId, $select = '*')
+    {
+        $res = $this->db->select($select)
+            ->from('area_junction_relation')
+            ->where('area_id', $areaId)
+            ->where('delete_at', '1970-01-01 00:00:00')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->result_array() : $res;
+    }
+
+    /**
+     * 删除区域
+     *
+     * @param $areaId
+     * @return bool
+     */
+    public function deleteArea($areaId)
+    {
+        return $this->db->where('id', $areaId)
+            ->set('delete_at', date('Y-m-d H:i:s'))
+            ->update($this->tb);
+    }
+
+    /**
+     * 删除区域路口关联
+     *
+     * @param $areaId
+     * @param $junctionIds
+     * @return null
+     */
+    public function deleteAreaJunctions($areaId, $junctionIds)
+    {
+        return $this->db->where('area_id', $areaId)
+            ->where_in('junction_id', $junctionIds)
+            ->where('delete_at', '1970-01-01 00:00:00')
+            ->set('delete_at', date('Y-m-d H:i:s'))
+            ->update('area_junction_relation');
+    }
+
+    /**
+     * 根据区域ID集合获取
+     *
+     * @param $areaIds
+     * @param string $select
+     * @return array
+     */
+    public function getAreaJunctionsByAreaIds($areaIds, $select = '*')
+    {
+        $res = $this->db->select($select)
+            ->from('area_junction_relation')
+            ->where_in('area_id', $areaIds)
+            ->where('delete_at', '1970-01-01 00:00:00')
+            ->get();
+
+        return $res instanceof CI_DB_result ? $res->result_array() : $res;
+    }
+
+    public function areaNameIsUnique($areaName, $cityId, $areaId = null)
+    {
+        $this->db->limit(1)
+            ->from($this->tb)
+            ->where('area_name', $areaName)
+            ->where('city_id', $cityId)
+            ->where('delete_at != ', '1970-01-01 00:00:00');
+
+        if(!is_null($areaId)) {
+            $this->db->where('area_id != ', $areaId);
+        }
+        if(!empty($this->db->get()) && method_exists($this->db->get(),"num_rows")){
+            return $this->db->get()->num_rows() === 0;
+        }else{
+            return true;
+        }
+    }
+}
---
+++ application/services/AdpAreaService.php
@@ -0,0 +1,499 @@
+<?php
+/**
+ * 信控平台 - 区域相关接口
+ *
+ * User: lichaoxi_i@didichuxing.com
+ */
+
+namespace Services;
+
+use Didi\Cloud\Collection\Collection;
+
+/**
+ * Class AreaService
+ * @package Services
+ * @property \Area_model $area_model
+ */
+class AdpAreaService extends BaseService
+{
+    /**
+     * AreaService constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('waymap_model');
+        $this->load->model('adpArea_model');
+        $this->load->model('redis_model');
+
+        $this->load->config('nconf');
+    }
+
+    /**
+     * 获取区域列表
+     *
+     * @param $params
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getList($params)
+    {
+
+        $cityId = $params['city_id'];
+
+        $areaList = $this->adpArea_model->getAreasByCityId($cityId);
+
+        return [
+            'list' => $areaList,
+        ];
+    }
+
+    /**
+     * 添加区域
+     *
+     * @param $params
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function addArea($params)
+    {
+        $cityId      = $params['city_id'];
+        $areaName    = $params['area_name'];
+        $junctionIds = $params['junction_ids'];
+
+        $data = [
+            'area_name' => $areaName,
+            'city_id' => $cityId,
+        ];
+
+        if (!$this->area_model->areaNameIsUnique($areaName, $cityId)) {
+            throw new \Exception('区域名称 ' . $areaName . ' 已经存在', ERR_DATABASE);
+        }
+
+        // 创建区域
+        $areaId = $this->area_model->insertArea($data);
+
+        // 创建区域路口关联
+        $failCount = $this->area_model->insertAreaJunctions($areaId, $junctionIds);
+
+        if ($failCount === 0) {
+            throw new \Exception('插入区域路口失败', ERR_PARAMETERS);
+        }
+
+        return $areaId;
+    }
+
+    /**
+     * 更新区域
+     *
+     * @param $params
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function updateArea($params)
+    {
+        $areaId      = $params['area_id'];
+        $areaName    = $params['area_name'];
+        $junctionIds = $params['junction_ids'];
+
+        // 获取区域信息
+        $areaInfo = $this->area_model->getAreaByAreaId($areaId, 'id');
+
+        if (!$areaInfo || empty($areaInfo)) {
+            throw new \Exception('目标区域不存在', ERR_PARAMETERS);
+        }
+
+        $areaId = $areaInfo['id'];
+        $cityId      = $areaInfo['city_id'];
+
+        $data = [
+            'area_name' => $areaName,
+        ];
+
+        if (!$this->area_model->areaNameIsUnique($areaName, $cityId, $areaId)) {
+            throw new \Exception('区域名称 ' . $areaName . ' 已经存在', ERR_DATABASE);
+        }
+
+        // 更新区域信息
+        $res = $this->area_model->updateArea($areaId, $data);
+
+        if (!$res) {
+            throw new \Exception('更新区域失败', ERR_PARAMETERS);
+        }
+
+        $areaJunctionList = $this->area_model->getAreaJunctions($areaId, 'junction_id');
+
+        $oldJunctionIds = array_column($areaJunctionList, 'junction_id');
+        $newJunctionIds = $junctionIds;
+
+        $shouldDeleted = array_diff($oldJunctionIds, $newJunctionIds);
+        $shouldCreated = array_diff($newJunctionIds, $oldJunctionIds);
+
+        $this->area_model->insertAreaJunctions($areaId, $shouldCreated);
+        $this->area_model->deleteAreaJunctions($areaId, $shouldDeleted);
+
+        return $areaId;
+    }
+
+    /**
+     * 删除区域
+     *
+     * @param $params
+     *
+     * @return mixed
+     */
+    public function deleteArea($params)
+    {
+        $areaId = $params['area_id'];
+
+        return $this->area_model->deleteArea($areaId);
+    }
+
+    /**
+     * 获取指定区域详情
+     *
+     * @param $params
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function getAreaDetail($params)
+    {
+        //$cityId = $params['city_id'];
+        $areaId = $params['area_id'];
+
+        $areaInfo = $this->area_model->getAreaByAreaId($areaId);
+
+        if (!$areaInfo) {
+            throw new \Exception(' 目标区域不存在', ERR_PARAMETERS);
+        }
+
+        $areaJunctionList = $this->area_model->getAreaJunctions($areaId);
+
+        $areaJunctionCollection = Collection::make($areaJunctionList);
+
+        // 取小数后6位
+        $round = function ($item) {
+            return round($item, 6);
+        };
+
+        $junctionIds = $areaJunctionCollection->implode('junction_id', ',');
+
+        $junctionInfoList = $this->waymap_model->getJunctionInfo($junctionIds);
+
+        $junctionInfoCollection = Collection::make($junctionInfoList);
+
+        $centerLat = $junctionInfoCollection->avg('lat', $round);
+        $centerLng = $junctionInfoCollection->avg('lng', $round);
+
+        return [
+            'center_lat' => $centerLat,
+            'center_lng' => $centerLng,
+            'area_id' => $areaId,
+            'area_name' => $areaInfo['area_name'] ?? '',
+            'junction_list' => $junctionInfoList,
+        ];
+    }
+
+    /**
+     * 获取城市全部区域详情
+     * @param $params['city_id'] int 城市ID
+     * @return array
+     * @throws \Exception
+     */
+    public function getCityAreaDetail($params)
+    {
+        $cityId = $params['city_id'];
+
+        // 获取城市全部区域信息
+        $areaList       = $this->area_model->getAreasByCityId($cityId, 'id, area_name');
+        if (empty($areaList)) {
+            return (object)[];
+        }
+        $areaCollection = Collection::make($areaList);
+
+        // 获取区域ID
+        $areaIdList = $areaCollection->column('id')->get();
+
+        // 获取 ID 和 名称的映射
+        $areaIdToNameList = $areaCollection->column('area_name', 'id')->get();
+
+        // 获取全部区域路口映射
+        $areaJunctionList       = $this->area_model->getAreaJunctionsByAreaIds($areaIdList);
+        $areaJunctionCollection = Collection::make($areaJunctionList);
+
+        // 从路网获取路口信息
+        $junctionIds    = $areaJunctionCollection->implode('junction_id', ',');
+        $junctionList   = $this->waymap_model->getJunctionInfo($junctionIds);
+        $junctionIdList = array_column($junctionList, null, 'logic_junction_id');
+
+        $areaIdJunctionList = $areaJunctionCollection
+            ->groupBy('area_id', function ($item) {
+                return array_column($item, 'junction_id');
+            })->krsort();
+
+        $results = [];
+
+        foreach ($areaIdJunctionList as $areaId => $junctionIds) {
+
+            $junctionCollection = Collection::make([]);
+
+            foreach ($junctionIds as $id) {
+                $junctionCollection[] = $junctionIdList[$id] ?? '';
+            }
+
+            $results[] = [
+                'area_id' => $areaId,
+                'area_name' => $areaIdToNameList[$areaId] ?? '',
+                'center_lat' => $junctionCollection->avg('lat'),
+                'center_lng' => $junctionCollection->avg('lng'),
+                'junction_list' => $junctionCollection,
+            ];
+        }
+
+        return $results;
+    }
+
+    /**
+     * 获取区域指标
+     *
+     * @return mixed
+     */
+    public function getQuotas()
+    {
+        return $this->config->item('area');
+    }
+
+    /**
+     * 区域指标评估
+     *
+     * @param $params
+     *
+     * @return array|mixed
+     * @throws \Exception
+     */
+    public function comparison($params)
+    {
+        $areaId            = $params['area_id'];
+        $quotaKey          = $params['quota_key'];
+        $cityId            = $params['city_id'];
+        $baseStartDate     = $params['base_start_date'];
+        $baseEndDate       = $params['base_end_date'];
+        $evaluateStartDate = $params['evaluate_start_date'];
+        $evaluateEndDate   = $params['evaluate_end_date'];
+
+        // 指标算法映射
+        $methods = [
+            'speed' => 'round(avg(speed) * 3.6, 2) as speed',
+            'stop_delay' => 'round(avg(stop_delay), 2) as stop_delay',
+        ];
+
+        // 名称配置
+        $nameMaps = $this->config->item('area_map');
+
+        // 获取指标单位
+        $units = array_column($this->config->item('area'), 'unit', 'key');
+
+        // 指标不存在与映射数组中
+        if (!isset($methods[$quotaKey])) {
+            throw new \Exception('指标不存在', ERR_PARAMETERS);
+        }
+
+        $areaInfo = $this->area_model->getAreaByAreaId($areaId);
+
+        if (!$areaInfo) {
+            throw new \Exception('该区域已被删除', ERR_PARAMETERS);
+        }
+
+        // 获取该区域全部路口ID
+        $junctionList = $this->area_model->getJunctionsByAreaId($areaId, 'junction_id');
+
+        // 数据获取失败 或者 数据为空
+        if (!$junctionList || empty($junctionList)) {
+            throw new \Exception('路口数据获取失败', ERR_PARAMETERS);
+        }
+
+        $junctionCollection = Collection::make($junctionList);
+
+        $junctionIds = $junctionCollection->column('junction_id')->get();
+
+        // 基准、评估时间范围
+        $baseDates     = dateRange($baseStartDate, $baseEndDate);
+        $evaluateDates = dateRange($evaluateStartDate, $evaluateEndDate);
+
+        // 生成 00:00 - 23:30 间的 粒度为 30 分钟的时间集合数组
+        $hours = hourRange();
+
+        // 获取数据
+        $select = 'date, hour, ' . $methods[$quotaKey];
+        $dates  = array_merge($baseDates, $evaluateDates);
+
+        $resultList = $this->area_model->getJunctionByCityId($dates, $junctionIds, $hours, $cityId, $select);
+        if (!$resultList || empty($resultList)) {
+            return  [];
+        }
+
+        $resultCollection = Collection::make($resultList);
+
+        // 将数据按照 日期（基准 和 评估）进行分组的键名函数
+        $baseOrEvaluateCallback = function ($item) use ($baseDates) {
+            return in_array($item['date'], $baseDates) ? 'base' : 'evaluate';
+        };
+
+        // 数据分组后，将每组数据进行处理的函数
+        $groupByItemFormatCallback = function ($item) use ($params, $hours) {
+            $hourToNull  = array_combine($hours, array_fill(0, 48, null));
+            $item        = array_column($item, $params['quota_key'], 'hour');
+            $hourToValue = array_merge($hourToNull, $item);
+
+            $result = [];
+
+            foreach ($hourToValue as $hour => $value) {
+                $result[] = [$hour, $value];
+            }
+
+            return $result;
+        };
+
+        // 数据处理
+        $result = $resultCollection
+            ->groupBy([$baseOrEvaluateCallback, 'date'], $groupByItemFormatCallback)
+            ->get();
+
+        $result['info'] = [
+            'area_name' => $areaInfo['area_name'],
+            'quota_name' => $nameMaps[$quotaKey] ?? '',
+            'quota_unit' => $units[$quotaKey] ?? '',
+            'base_time' => [$baseStartDate, $baseEndDate],
+            'evaluate_time' => [$evaluateStartDate, $evaluateEndDate],
+        ];
+
+        // 构建 Redis Key
+        $downloadId                    = md5(json_encode($result));
+        $result['info']['download_id'] = $downloadId;
+
+        $this->redis_model->setComparisonDownloadData($downloadId, $result);
+
+        return $result;
+    }
+
+    /**
+     * 获取区域评估下载地址
+     *
+     * @param $params
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function downloadEvaluataData($params)
+    {
+        $downloadId = $params['download_id'];
+
+        $key = $this->config->item('quota_evaluate_key_prefix') . $downloadId;
+
+        if (!$this->redis_model->getData($key)) {
+            throw new \Exception('请先评估再下载', ERR_PARAMETERS);
+        }
+
+        return [
+            'download_url' => $this->config->item('area_download_url_prefix') . $params['download_id'],
+        ];
+
+    }
+
+    /**
+     * 评估数据文件下载
+     *
+     * @param $params
+     *
+     * @throws \Exception
+     * @throws \PHPExcel_Exception
+     * @throws \PHPExcel_Writer_Exception
+     */
+    public function download($params)
+    {
+        $downloadId = $params['download_id'];
+
+        $excelStyle = $this->config->item('excel_style');
+
+        $data = $this->redis_model->getComparisonDownloadData($downloadId);
+
+        if (!$data) {
+            throw new \Exception('请先评估再下载', ERR_PARAMETERS);
+        }
+
+        $fileName = "{$data['info']['area_name']}_" . date('Ymd');
+
+        $objPHPExcel = new \PHPExcel();
+        $objSheet    = $objPHPExcel->getActiveSheet();
+        $objSheet->setTitle('数据');
+
+        $detailParams = [
+            ['指标名', $data['info']['quota_name']],
+            ['方向', $data['info']['direction'] ?? ''],
+            ['基准时间', implode(' ~ ', $data['info']['base_time'])],
+            ['评估时间', implode(' ~ ', $data['info']['evaluate_time'])],
+            ['指标单位', $data['info']['quota_unit']],
+        ];
+
+        $objSheet->mergeCells('A1:F1');
+        $objSheet->setCellValue('A1', $fileName);
+        $objSheet->fromArray($detailParams, null, 'A4');
+
+        $objSheet->getStyle('A1')->applyFromArray($excelStyle['title']);
+        $rows_idx = count($detailParams) + 3;
+        $objSheet->getStyle("A4:A{$rows_idx}")->getFont()->setSize(12)->setBold(true);
+
+        $line = 6 + count($detailParams);
+
+        if (!empty($data['base'])) {
+
+            $table = getExcelArray($data['base']);
+
+            $objSheet->fromArray($table, null, 'A' . $line);
+
+            $rows_cnt   = count($table);
+            $cols_cnt   = count($table[0]) - 1;
+            $rows_index = $rows_cnt + $line - 1;
+
+            $objSheet->getStyle("A{$line}:" . intToChr($cols_cnt) . $rows_index)->applyFromArray($excelStyle['content']);
+            $objSheet->getStyle("A{$line}:A{$rows_index}")->applyFromArray($excelStyle['header']);
+            $objSheet->getStyle("A{$line}:" . intToChr($cols_cnt) . $line)->applyFromArray($excelStyle['header']);
+
+            $line += ($rows_cnt + 2);
+        }
+
+        if (!empty($data['evaluate'])) {
+
+            $table = getExcelArray($data['evaluate']);
+
+            $objSheet->fromArray($table, null, 'A' . $line);
+
+            $rows_cnt   = count($table);
+            $cols_cnt   = count($table[0]) - 1;
+            $rows_index = $rows_cnt + $line - 1;
+            //$objSheet->getStyle("A≈{$line}:" . intToChr($cols_cnt) . $rows_index)->applyFromArray($excelStyle['content']);
+            $objSheet->getStyle("A{$line}:A{$rows_index}")->applyFromArray($excelStyle['header']);
+            $objSheet->getStyle("A{$line}:" . intToChr($cols_cnt) . $line)->applyFromArray($excelStyle['header']);
+        }
+
+        $objWriter = new \PHPExcel_Writer_Excel5($objPHPExcel);
+
+        header('Content-Type: application/x-xls;');
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename=' . $fileName . '.xls');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1');
+        header('Expires: 0'); // Date in the past
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+        ob_end_clean();
+        $objWriter->save('php://output');
+        exit();
+    }
+}
