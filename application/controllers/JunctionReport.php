e32bfbef8eb0df9b7a64165a84cad7c452c285a3
zhuyewei <zhuyewei@didiglobal.com>
2019-10-26 17:07:15 +0800

    重点路口分析功能

--- application/controllers/JunctionReport.php
+++ application/controllers/JunctionReport.php
@@ -6,10 +6,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 use Services\JunctionService;
+use Services\JunctionReportService;
 
 class JunctionReport extends MY_Controller
 {
     protected $junctionService;
+    protected $junctionReportService;
 
     public function __construct()
     {
@@ -18,6 +20,7 @@ class JunctionReport extends MY_Controller
         $this->config->load('report_conf');
 
         $this->junctionService = new JunctionService();
+        $this->junctionReportService = new JunctionReportService();
     }
 
     /**
@@ -58,8 +61,47 @@ class JunctionReport extends MY_Controller
 
     }
 
+    /*路口运行指标分析*/
     public function queryJuncQuotaAnalysis(){
+        $params = $this->input->get(null, true);
 
+        $this->get_validate([
+            'city_id' => 'required|is_natural_no_zero',
+            'logic_junction_id' => 'required|min_length[1]',
+            'start_time'     => 'required|trim|regex_match[/\d{4}-\d{2}-\d{2}/]',
+            'end_time'       => 'required|trim|regex_match[/\d{4}-\d{2}-\d{2}/]',
+        ],$params);
+
+        $quotaData = $this->junctionReportService->queryJuncQuotaDetail($params['city_id'],$params['logic_junction_id'],$params['start_time'],$params['end_time']);
+        //数据聚合
+        $flowQuota=[];
+        foreach ($quotaData['quota_info'] as $qk=>$qv){
+            $flowQuota[$qk]=[];
+            foreach ($qv as $yk=>$yv){
+                foreach ($yv as $d){
+                    if(!isset($flowQuota[$qk][$d['hour']])){
+                        $flowQuota[$qk][$d['hour']]=[
+                            'traj_count'=>0,
+                            'speed'=>0,
+                            'stop_delay'=>0,
+                            'stop_time_cycle'=>0,
+                        ];
+                    }
+                    $flowQuota[$qk][$d['hour']]['traj_count']+=$d['traj_count'];
+                    $flowQuota[$qk][$d['hour']]['speed']+=$d['speed']*$d['traj_count'];
+                    $flowQuota[$qk][$d['hour']]['stop_delay']+=$d['stop_delay']*$d['traj_count'];
+                    $flowQuota[$qk][$d['hour']]['stop_time_cycle']+=$d['stop_time_cycle']*$d['traj_count'];
+                }
+            }
+        }
+        //数据转图表
+        $chartData = $this->junctionReportService->trans2Chart($flowQuota,$quotaData['flow_info']);
+        $finalData = $this->junctionReportService->chartAnalysis($chartData);
+        $finalData = [
+            "overview"=>"路口各个转向的运行指标(包括停车次数、停车延误、行驶速度、排队长度等指标)变化情况如下图所示。",
+            "quotalist"=>$finalData,
+        ];
+        $this->response($finalData);
     }
 
 
