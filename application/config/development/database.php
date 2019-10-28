a76849a8f3fd2c4372db43384fb2ca1bde658733
zhuyewei <zhuyewei@didiglobal.com>
2019-10-28 11:52:28 +0800

    新增pi数据连接

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -155,6 +155,29 @@ $db['signal_control'] = array(
     'save_queries' => TRUE
 );
 
+$db['sts_index'] = array(
+    'dsn'   => '',
+    'username' => $username,
+    'hostname' => $hostname,
+    'password' => $password,
+    'port'     => $port,
+    'database' => 'sts_index',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    // 'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+
 
 $db['its_trace_log'] = array(
     'dsn'   => '',
