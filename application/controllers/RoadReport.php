28144d110aae583f78369a21f26aa897835dc742
lizhaohua <lizhaohua@didichuxing.com>
2019-10-31 13:32:38 +0800

    [fix] chart to charts

--- application/controllers/RoadReport.php
+++ application/controllers/RoadReport.php
@@ -115,7 +115,7 @@ class RoadReport extends MY_Controller
         $desc="下图利用滴滴数据绘制了该干线全天24小时各项运行指标(车均停车次数、车均停车延误、车均行驶速度、PI)。通过数据分析,该干线的早高峰约为".$mrushTime['s']."~".$mrushTime['e'].",晚高峰约为".$erushTime['s']."~".$erushTime['e']."。与平峰相比,早晚高峰的停车次数达到".round(($avgStopCyclem+$avgStopCyclee)/2,2)."次/车/路口,停车延误接近".round(($avgStopDelaym+$avgStopDelaye)/2,2)."秒/车/路口,车均行驶速度也达到".round(($avgSpeedm+$avgSpeede)/2,2)."千米/小时左右";
 
 
-        $this->response(['info'=>['instructions'=>"报告采用综合评估指数（PI）来分析路口整体及各维度交通运行情况XXXX",'desc'=>$desc,'morning_rush_time'=>$mrushTime['s']."~".$mrushTime['e'],"evening_rush_time"=>$erushTime['s']."~".$erushTime['e']],'chart'=>$chartDatas]);
+        $this->response(['info'=>['instructions'=>"报告采用综合评估指数（PI）来分析路口整体及各维度交通运行情况XXXX",'desc'=>$desc,'morning_rush_time'=>$mrushTime['s']."~".$mrushTime['e'],"evening_rush_time"=>$erushTime['s']."~".$erushTime['e']],'charts'=>$chartDatas]);
 
     }
 
