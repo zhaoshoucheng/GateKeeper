fe62688cadc797e5af94deb0703047b56536f336
zhuyewei <zhuyewei@didiglobal.com>
2019-11-18 15:25:44 +0800

    快速路

---
+++ application/controllers/Expressway.php
@@ -0,0 +1,53 @@
+<?php
+
+
+/***************************************************************
+# 快速路需求
+# user:zhuyewei@didichuxing.com
+# date:2019-11-18
+ ***************************************************************/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+
+class Expressway extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    /*
+     * 快速路概览
+     * */
+    public function overview(){
+
+    }
+
+    /*
+     * 快速路拥堵概览
+     * */
+    public function stopDelayTopList(){
+
+    }
+
+    /*
+     * 快速路指标详情
+     * */
+    public function detail(){
+
+    }
+
+    /*
+     * 快速路报警列表
+     * */
+    public function alarmlist(){
+
+    }
+
+
+
+
+}
