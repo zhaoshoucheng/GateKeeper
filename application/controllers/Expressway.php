8bff8b0130c675e3b0a8cac62f6053937317c55d
zhuyewei <zhuyewei@didiglobal.com>
2019-11-20 10:22:58 +0800

    bug fix

--- application/controllers/Expressway.php
+++ application/controllers/Expressway.php
@@ -9,13 +9,14 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
+use Services\ExpresswayService;
 
 class Expressway extends MY_Controller
 {
     public function __construct()
     {
         parent::__construct();
+        $this->expresswayService = new ExpresswayService();
 
     }
 
@@ -23,14 +24,30 @@ class Expressway extends MY_Controller
      * 快速路概览
      * */
     public function overview(){
+        $params = $this->input->post(null, true);
 
+        $this->validate([
+            'city_id' => 'required|is_natural_no_zero',
+        ]);
+        //查询路口列表
+        $data = $this->expresswayService->queryOverview($params['city_id']);
+
+        $this->response($data);
     }
 
     /*
      * 快速路拥堵概览
      * */
     public function stopDelayTopList(){
+        $params = $this->input->post(null, true);
 
+        $this->validate([
+            'city_id' => 'required|is_natural_no_zero',
+        ]);
+        //查询路口列表
+        $data = $this->expresswayService->queryOverview($params['city_id']);
+
+        $this->response($data);
     }
 
     /*
--- application/services/ExpresswayService.php
+++ application/services/ExpresswayService.php
@@ -13,6 +13,53 @@ class ExpresswayService extends BaseService
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('expressway_model');
+    }
+
+    public function queryOverview($cityID){
+        //查询匝道信息
+//        $juncInfos  = $this->expressway_model->getQuickRoadMovement($cityID);
+
+        //TODO 路口过滤
+
+//        $movements = $juncInfos['movements'];
+
+        //查询干线信息
+
+        $roadInfos = $this->expressway_model->getQuickRoadSegments($cityID);
+
+        $ret = [
+            'junc_list'=>[],
+            'road_list'=>[]
+        ];
+
+        foreach ($roadInfos['junctions'] as $j){
+            $ret['junc_list'][] = [
+                "junction_id"=>$j['junction_id'],
+                "lng"=>$j['lng'],
+                "lat"=>$j['lat'],
+                "type"=>$j['type']
+            ];
+        }
+
+        foreach ($roadInfos['segments'] as $s){
+            $ret['road_list'][] = [
+                "id"=>$s['segment_id'],
+                "start_junc"=>$s['start_junc_id'],
+                "end_junc"=>$s['end_junc_id'],
+                "length"=>$s['length'],
+                "name"=>$s['name'],
+                "geom"=>$s['gemo']
+            ];
+        }
+
+
+
+        return $ret;
+    }
+
+    public function queryStopDelayList(){
+
     }
 
 
