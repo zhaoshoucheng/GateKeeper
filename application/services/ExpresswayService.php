98dcae9efad1dbc88745b2a1091774447baa8f83
lizhaohua <lizhaohua@didichuxing.com>
2019-11-23 22:15:16 +0800

    [new] /expressway/condition

--- application/services/ExpresswayService.php
+++ application/services/ExpresswayService.php
@@ -6,6 +6,9 @@ namespace Services;
  * Class ExpresswayService
  * @package Services
  */
+
+use Services\DataService;
+
 class ExpresswayService extends BaseService
 {
 
@@ -14,6 +17,9 @@ class ExpresswayService extends BaseService
     {
         parent::__construct();
         $this->load->model('expressway_model');
+        $this->load->model('waymap_model');
+
+        $this->dataService = new DataService();
     }
 
     public function queryOverview($cityID){
@@ -67,10 +73,60 @@ class ExpresswayService extends BaseService
     }
 
     public function alarmlist($params) {
+    	$city_id = $params['city_id'];
+    	$es_data = $this->dataService->call("/expressway/Condition", [
+    		'city_id' => $city_id,
+    	], "POST", 'json');
+    	var_dump($es_data);
 
+    	$link_ids = array_column($es_data['list'], 'link_id');
+    	$version =$this->waymap_model->getLastMapVersion();
+    	$link_infos = $this->waymap_model->getLinksGeoInfos($link_ids, $version, true);
+    	return $link_infos;
+    	// $link_infos_map = [];
+    	// foreach ($link_info as $link_info) {
+
+    	// }
+
+    	// $ret = [
+    	// 	"trafficList" => [],
+    	// ];
+    	// foreach ($variable as $key => $value) {
+    	// 	# code...
+    	// }
     }
 
     public function condition($params) {
+    	$city_id = intval($params['city_id']);
+    	$es_data = $this->dataService->call("/expressway/Condition", [
+    		'city_id' => $city_id,
+    	], "POST", 'json');
 
+    	$list = $es_data[2]['list'];
+    	$link_ids = array_column($list, 'link_id');
+    	$version =$this->waymap_model->getLastMapVersion();
+    	$link_infos = $this->waymap_model->getLinksGeoInfos($link_ids, $version, true);
+    	$link_geom_map = [];
+    	foreach ($link_infos['features'] as $link_info) {
+    		if (in_array($link_info['properties']['id'], $link_ids)) {
+    			$link_geom_map[$link_info['properties']['id']] = $link_info['geometry'];
+    		}
+    	}
+
+
+    	foreach ($list as $key => $value) {
+    		if (isset($link_geom_map[$value['link_id']])) {
+    			$list[$key]['geom'] = $link_geom_map[$value['link_id']];
+    		} else {
+    			$list[$key]['geom'] = [
+    				"coordinates" => [],
+    				"type" => "LineString",
+    			];
+    		}
+    	}
+    	return [
+    		'trafficList' => $list,
+    		'hms' => $es_data[2]['hms'],
+    	];
     }
 }
