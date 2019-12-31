128a0dfa7951523aa6814b71b91d390c27fbd100
zhuyewei <zhuyewei@didiglobal.com>
2019-12-31 10:55:26 +0800

    新增excel功能

---
+++ application/controllers/DownloadSpreadsheet.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: didi
+ * Date: 2019/12/27
+ * Time: 下午3:19
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use Services\SpreadsheetService;
+
+
+class DownloadSpreadsheet extends MY_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->SpreadsheetService = new SpreadsheetService();
+    }
+
+    /*
+     * file 图片
+     * value:{
+     *  "logic_junction_id":"",
+     *  "junction_name":"",
+     *  "opt_rets":[
+     *      {
+     *          "name":""
+     *          "start_time":"",
+     *          "end_time":""
+     *      }
+     *  ]
+     * }
+     * */
+    public function SingleTimeOpt(){
+        $params = $this->input->post(null, true);
+
+        $obj = json_decode($params['value'],true);
+
+        $this->SpreadsheetService->checkUploadFile($_FILES);
+
+        $this->SpreadsheetService->SingleTimeOptSpreadsheet($_FILES,$obj);
+
+    }
+
+    /*
+    * file 图片
+    * value:{
+    *  "logic_junction_id":"",
+    *  "junction_name":"",
+    *  "time":"",
+     * "cycle":100,
+     * "offset":10,
+    *  "opt_rets":[
+    *      {
+    *          "name":""
+    *          "start_time":"",
+    *          "end_time":"",
+     *         "yellow":"",
+     *         "green_length":1
+    *      }
+    *  ]
+    * }
+    * */
+    public function SingleGreenOpt(){
+        $params = $this->input->post(null, true);
+        $obj = json_decode($params['value'],true);
+
+        $this->SpreadsheetService->checkUploadFile($_FILES);
+
+        $this->SpreadsheetService->SingleGreenOptSpreadsheet($_FILES,$obj);
+    }
+
+    /*
+       * file 图片
+       * value:{
+       *  "road_id":"",
+       *  "road_name":"",
+       *  "opt_rets":[
+       *      {
+       *          "direction":"",
+       *          "junction_info":{
+     *                "name":"aa",
+       *              "cycle":1,
+       *              "offset":1,
+       *              "start_time":1,
+       *              "green_length":1,
+       *          }
+       *      }
+       *  ]
+       * }
+   * */
+    public function RoadTrafficOpt(){
+        $params = $this->input->post(null, true);
+        $obj = json_decode($params['value'],true);
+
+        $this->SpreadsheetService->checkUploadFile($_FILES);
+
+        $this->SpreadsheetService->RoadTrafficOptSpreadsheet($_FILES,$obj);
+    }
+
+
+
+}
