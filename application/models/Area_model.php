8f39b23db2f589e83a0bbce99b8c714655b15210
zhuyewei <zhuyewei@didiglobal.com>
2020-04-13 16:59:11 +0800

    bug fix

--- application/models/Area_model.php
+++ application/models/Area_model.php
@@ -153,7 +153,7 @@ class Area_model extends CI_Model
                    "stop_time_cycle"=> round($hv['stop_time_cycle']['value']/$hv['traj_count']['value'],2),
                ];
             }
-        }
+
         return $finalRet;
 
     }
