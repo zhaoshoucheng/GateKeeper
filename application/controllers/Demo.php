092a3bce1a969e275f162074cde9b9631df9a82d
niuyufu <niuyufu@didichuxing.com>
2020-05-07 16:53:44 +0800

    代码暂存

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -40,12 +40,12 @@ class Demo extends MY_Controller
             return [];
         }
         $junctionIDs = [];
-        foreach ($response["aggregations"]["segment_id"]["buckets"] as $agg) {
-            $junctionIDs[] = $agg["key"];
-        }
-
+        // foreach ($response["aggregations"]["segment_id"]["buckets"] as $agg) {
+        // $junctionIDs[] = $agg["key"];
+        // }
         // $junctionInfos = $this->expressway_model->getQuickRoadSegmentsByJunc("23", $junctionIDs);
-        print_r($junctionIDs);
+
+        print_r($response);
         exit;
         $junctionIDs = array_column($ret, 'junction_id');
         $junctionInfos = $this->expressway_model->getQuickRoadSegmentsByJunc($cityID, $junctionIDs);
