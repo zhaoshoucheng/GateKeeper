295f0f267668248294b86b6d22fd0d51629f898b
niuyufu <niuyufu@didichuxing.com>
2020-05-07 18:36:32 +0800

    代码暂存

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -83,7 +83,7 @@ class Demo extends MY_Controller
             }
             print_r($param[0] . " - " . $param[1]);
             echo "\n";
-            print_r(json_encode(array_slice($outputList, 0, 30), JSON_UNESCAPED_UNICODE));
+            print_r(json_encode(array_slice($outputList, 0, 30), 256));
         }
         exit;
         $resPart = json_decode($response, true);
