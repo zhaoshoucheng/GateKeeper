d85e70048cccc1507db102959f192dda8873be8e
niuyufu <niuyufu@didichuxing.com>
2020-05-07 18:37:16 +0800

    代码暂存

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -84,6 +84,7 @@ class Demo extends MY_Controller
             print_r($param[0] . " - " . $param[1]);
             echo "\n";
             print_r(json_encode(array_slice($outputList, 0, 30), 256));
+            echo "\n";
         }
         exit;
         $resPart = json_decode($response, true);
