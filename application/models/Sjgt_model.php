05d877743282ef65e2148415846a747eb6783661
niuyufu <niuyufu@didichuxing.com>
2020-05-09 20:05:51 +0800

    sjgt

--- application/models/Sjgt_model.php
+++ application/models/Sjgt_model.php
@@ -50,7 +50,7 @@ class Sjgt_model extends CI_Model
             print_r($item);
             echo "INSERT INTO `area` (`id`, `area_name`, `city_id`, `user_id`, `update_at`, `create_at`, `delete_at`) VALUES (NULL, '" . $item["area_name"] . "', '12', '0', '2020-05-09 11:01:02', '2020-05-09 21:28:38', '1970-01-01 00:00:00');<br/>";
 
-            if ($coordinates["MultiPolygon"] == "MultiPolygon") {
+            if ($coordinates["type"] == "MultiPolygon") {
                 foreach ($coordinates["coordinates"] as $partCoordinate) {
                     foreach ($partCoordinate[0] as $point) {
                         $points[] = implode(",", $point);
