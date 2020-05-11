4a8af6d2b3afa69957e0a0fa786aa2625f4b412c
niuyufu <niuyufu@didichuxing.com>
2020-05-11 10:25:52 +0800

    添加crond

--- application/models/Sjgt_model.php
+++ application/models/Sjgt_model.php
@@ -43,12 +43,16 @@ class Sjgt_model extends CI_Model
             ->from($this->tb)
             ->order_by("id", "desc")
             ->get();
+        $this->db->trans_begin();
         $list = $res instanceof CI_DB_result ? $res->result_array() : $res;
         foreach ($list as $item) {
             $coordinates = json_decode($item["area_geometry"], true);
             $points = [];
-            print_r($item);
-            echo "INSERT INTO `area` (`id`, `area_name`, `city_id`, `user_id`, `update_at`, `create_at`, `delete_at`) VALUES (NULL, '" . $item["area_name"] . "', '12', '0', '2020-05-09 11:01:02', '2020-05-09 21:28:38', '1970-01-01 00:00:00');<br/>";
+            // print_r($item);
+            $insertSql =  "INSERT INTO `area` (`id`, `area_name`, `city_id`, `user_id`, `update_at`, `create_at`, `delete_at`) VALUES (NULL, '" . $item["area_name"] . "', '12', '0', '2020-05-09 11:01:02', '2020-05-09 21:28:38', '1970-01-01 00:00:00');<br/>";
+            $this->db->query($insertSql);
+            $areaID=$this->db->insertID();
+            echo $areaID;exit;
             if ($coordinates["type"] == "MultiPolygon") {
                 foreach ($coordinates["coordinates"] as $partCoordinate) {
                     foreach ($partCoordinate[0] as $point) {
@@ -67,7 +71,8 @@ class Sjgt_model extends CI_Model
                     // print_r(json_decode($ret, true));
 
                     foreach ($polygonResponse["data"]["filter_juncs"] as $juncItem) {
-                        echo "INSERT INTO `area_junction_relation` (`id`, `area_id`, `junction_id`, `user_id`, `update_at`, `create_at`, `delete_at`) VALUES (NULL, '175', '" . $juncItem["logic_junction_id"] . "', '0', '2019-12-05 10:28:40', '2019-12-05 10:28:40', '1970-01-01 00:00:00');<br/>";
+                        $insertSql = "INSERT INTO `area_junction_relation` (`id`, `area_id`, `junction_id`, `user_id`, `update_at`, `create_at`, `delete_at`) VALUES (NULL, '".$areaID."', '" . $juncItem["logic_junction_id"] . "', '0', '2020-05-11 10:28:40', '2020-05-11 10:28:40', '1970-01-01 00:00:00');";
+                        $this->db->query($insertSql);
                     }
                 }
             }
